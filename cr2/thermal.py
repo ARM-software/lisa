952815a1c803a35f0dacbef591dcd9440d754408
Javi Merino <javi.merino@arm.com>
2014-03-31 18:08:32 +0100

    thermal: don't create the intermediate thermal.csv file if all we want is the DataFrame

--- cr2/thermal.py
+++ cr2/thermal.py
@@ -3,12 +3,14 @@
 
 import os
 import re
+from StringIO import StringIO
 import pandas as pd
 
 class Thermal(object):
     def __init__(self):
         if not os.path.isfile("trace.txt"):
             self.__run_trace_cmd_report()
+        self.data_csv = ""
         self.data_frame = False
 
 
@@ -22,45 +24,53 @@ class Thermal(object):
         with open("trace.txt", "w") as f:
             f.write(out)
 
-    def write_thermal_csv(self):
+    def __parse_into_csv(self):
+        """Create a csv representation of the thermal data and store it in self.data_csv"""
         pat_timestamp = re.compile(r"([0-9]+\.[0-9]+):")
         pat_data = re.compile(r"[A-Za-z0-9_]+=([0-9]+) ")
         header = ""
 
+        with open("trace.txt") as fin:
+            for line in fin:
+                if not re.search("Ptot_out", line):
+                    continue
+
+                line = line[:-1]
+
+                m = re.search(pat_timestamp, line)
+                timestamp = m.group(1)
+
+                semi_idx = line.index(" : ")
+                data_str = line[semi_idx + 3:]
+
+                if not header:
+                    header = re.sub(r"([A-Za-z0-9_]+)=[0-9]+ ", r"\1,", data_str)
+                    header = header[:-1]
+                    header = "time," + header + "\n"
+                    self.data_csv = header
+
+                parsed_data = re.sub(pat_data, r"\1,", data_str)
+                # Drop the last comma
+                parsed_data = parsed_data[:-1]
+
+                parsed_data = timestamp + "," + parsed_data + "\n"
+                self.data_csv += parsed_data
+
+    def write_thermal_csv(self):
+        """Write the csv info in thermal.csv"""
+        if not self.data_csv:
+            self.__parse_into_csv()
+
         with open("thermal.csv", "w") as fout:
-            with open("trace.txt") as fin:
-                for line in fin:
-                    if not re.search("Ptot_out", line):
-                        continue
-
-                    line = line[:-1]
-
-                    m = re.search(pat_timestamp, line)
-                    timestamp = m.group(1)
-
-                    semi_idx = line.index(" : ")
-                    data_str = line[semi_idx + 3:]
-
-                    if not header:
-                        header = re.sub(r"([A-Za-z0-9_]+)=[0-9]+ ", r"\1,", data_str)
-                        header = header[:-1]
-                        header = "time," + header + "\n"
-                        fout.write(header)
-
-                    parsed_data = re.sub(pat_data, r"\1,", data_str)
-                    # Drop the last comma
-                    parsed_data = parsed_data[:-1]
-
-                    parsed_data = timestamp + "," + parsed_data + "\n"
-                    fout.write(parsed_data)
+            fout.write(self.data_csv)
 
     def get_data_frame(self):
         """Return a pandas data frame for the run"""
         if self.data_frame:
             return self.data_frame
 
-        if not os.path.isfile("thermal.csv"):
-            self.write_thermal_csv()
+        if not self.data_csv:
+            self.__parse_into_csv()
 
-        self.data_frame = pd.read_csv("thermal.csv").set_index("time")
+        self.data_frame = pd.read_csv(StringIO(self.data_csv)).set_index("time")
         return self.data_frame
