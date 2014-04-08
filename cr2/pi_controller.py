681c72cd8800298eed1bdfa2b403bacac6eb46be
Javi Merino <javi.merino@arm.com>
2014-04-08 12:58:46 +0100

    pi_controller: fix pylint complaints

--- cr2/pi_controller.py
+++ cr2/pi_controller.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
-"""Process the output of the power allocator's PI controller in the current directory's trace.dat"""
+"""Process the output of the power allocator's PI controller in the
+current directory's trace.dat"""
 
-import pandas as pd
 from thermal import BaseThermal
 
 class PIController(BaseThermal):
+    """Process the power allocator PI controller data in a ftrace dump"""
     def __init__(self, path=None):
         super(PIController, self).__init__(
             basepath=path,
