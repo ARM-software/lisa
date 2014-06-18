4a821d8a953dc7f7af1ec9b8d043ae1bf92f6156
Javi Merino <javi.merino@arm.com>
2014-06-18 15:08:50 +0100

    cr2: add utilisation plot to the summary plots

--- cr2/__init__.py
+++ cr2/__init__.py
@@ -15,6 +15,7 @@ def summary_plots(**kwords):
         path = None
 
     gov_data = ThermalGovernor(path=path)
+    inpower_data = InPower(path=path)
     pid_data = PIDController(path=path)
 
     if "width" not in kwords:
@@ -22,7 +23,11 @@ def summary_plots(**kwords):
     if "height" not in kwords:
         kwords["height"] = 5
 
+    # XXX This needs to be made generic
+    map_label = {0: "A7", 1: "A15"}
+
     gov_data.plot_temperature(**kwords)
+    inpower_data.plot_load(map_label, **kwords)
     pid_data.plot_controller(**kwords)
     gov_data.plot_input_power(**kwords)
     gov_data.plot_output_power(**kwords)
