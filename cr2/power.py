9237a3c9d4d70159740a137a7e5116ebdabe46da
Javi Merino <javi.merino@arm.com>
2014-06-18 12:18:51 +0100

    power: add a InPower.plot_load() method

--- cr2/power.py
+++ cr2/power.py
@@ -2,8 +2,11 @@
 """Process the output of the cpu_cooling devices in the current
 directory's trace.dat"""
 
-from thermal import BaseThermal
 from matplotlib import pyplot as plt
+import pandas as pd
+
+from thermal import BaseThermal
+from plot_utils import default_plot_settings
 
 class OutPower(BaseThermal):
     """Process the cpufreq cooling power actor data in a ftrace dump"""
@@ -28,6 +31,30 @@ class InPower(BaseThermal):
 
         return df[df["cluster"] == cluster]
 
+    def get_load_data(self, mapping_label):
+        """return a dataframe suitable for plot_load()
+
+        mapping_label is a dictionary mapping cluster numbers to labels."""
+
+        dfr = self.get_data_frame()
+        load_cols = [s for s in dfr.columns if s.startswith("load")]
+
+        load_series = dfr[load_cols[0]]
+        for col in load_cols[1:]:
+            load_series += dfr[col]
+
+        load_dfr = pd.DataFrame({"cluster": dfr["cluster"], "load": load_series})
+        cluster_numbers = set(dfr["cluster"])
+
+        ret_dict = {}
+        for num in cluster_numbers:
+            label = mapping_label[num]
+            cluster_load = load_dfr[dfr["cluster"] == num]["load"]
+
+            ret_dict[label] = cluster_load
+
+        return pd.DataFrame(ret_dict).fillna(method="pad")
+
     def plot_cluster_load(self, cluster):
         df = self.get_cluster_data_frame(cluster)
         load_cols = [s for s in df.columns if s.startswith("load")]
@@ -35,3 +62,18 @@ class InPower(BaseThermal):
         _, ax = plt.subplots()
         df[load_cols].plot(ax=ax)
         ax.set_ylim(0, 110)
+
+    def plot_load(self, mapping_label, title="Utilisation", width=None, height=None):
+        """plot the load of all the clusters, similar to how compare runs did it
+
+        the mapping_label has to be a dict whose keys are the cluster
+        numbers as found in the trace and values are the names that
+        will appear in the legend.
+
+        """
+
+        load_data = self.get_load_data(mapping_label)
+
+        self.init_fig(width=width, height=height)
+        load_data.plot(ax=self.ax)
+        default_plot_settings(self.ax, title=title)
--- tests/test_power.py
+++ tests/test_power.py
@@ -26,6 +26,18 @@ class TestPower(TestThermalBase):
         self.assertEquals(df["raw_cpu_power"].iloc[0], 36)
         self.assertTrue("load0" in df.columns)
 
+    def test_inpower_get_load_data(self):
+        """Test InPower.get_load_data()"""
+        load_data = InPower().get_load_data({0: "A7", 1: "A15"})
+
+        self.assertEquals(load_data["A15"].iloc[0], 2 + 6 + 0 + 1)
+        self.assertEquals(load_data["A7"].iloc[3], 9 + 7 + 20 + 2)
+        self.assertEquals(load_data["A15"].iloc[0], load_data["A15"].iloc[1])
+
     def test_inpower_plot_cluster_load(self):
         """Test that InPower.plot_cluster_load() doesn't explode"""
         InPower().plot_cluster_load(0)
+
+    def test_inpower_plot_load(self):
+        """Test that InPower.plot_load() doesn't explode"""
+        InPower().plot_load({0: "A7", 1: "A15"}, title="Util")
