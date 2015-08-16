88c368a1d15d9255a24a20bc2aaf821a72f0bfe7
Kapileshwar Singh <kapileshwar.singh@arm.com>
2015-08-17 00:43:25 +0100

    stats: Fix tests for pivots in grammar
    Pivoting of the grammar aggregated data requires a reference
    of the returned data from the parser's solver. This needs to
    be referenced against a pivot value.
    
    Signed-off-by: Kapileshwar Singh <kapileshwar.singh@arm.com>

--- tests/test_stats_grammar.py
+++ tests/test_stats_grammar.py
@@ -40,30 +40,33 @@ class TestStatsGrammar(BaseTestThermal):
     def test_accessors_sum(self):
         """Test Addition And Subtraction: Data"""
 
+        thermal_zone_id = 0
         parser = Parser(trappy.Run())
         # Equation with dataframe accessors
         eqn = "trappy.thermal.Thermal:temp + \
 trappy.thermal.Thermal:temp"
+
         assert_series_equal(
-            parser.solve(eqn),
+            parser.solve(eqn)[thermal_zone_id],
             2 *
             parser.data.thermal.data_frame["temp"])
 
     def test_funcparams_sum(self):
         """Test Addition And Subtraction: Functions"""
 
+        thermal_zone_id = 0
         parser = Parser(trappy.Run())
         # Equation with functions as parameters (Mixed)
         eqn = "numpy.mean(trappy.thermal.Thermal:temp) + 1000"
         self.assertEquals(
-            parser.solve(eqn),
+            parser.solve(eqn)[thermal_zone_id],
             np.mean(
                 parser.data.thermal.data_frame["temp"]) +
             1000)
         # Multiple func params
         eqn = "numpy.mean(trappy.thermal.Thermal:temp) + numpy.mean(trappy.thermal.Thermal:temp)"
         self.assertEquals(
-            parser.solve(eqn),
+            parser.solve(eqn)[thermal_zone_id],
             np.mean(
                 parser.data.thermal.data_frame["temp"]) *
             2)
@@ -71,34 +74,33 @@ trappy.thermal.Thermal:temp"
     def test_bool_ops_vector(self):
         """Test Logical Operations: Vector"""
 
+        thermal_zone_id = 0
         # The equation returns a vector mask
         parser = Parser(trappy.Run())
-        eqn = "(trappy.thermal.Thermal:temp > 40000) & (trappy.cpu_power.CpuOutPower:power > 800)"
+        eqn = "(trappy.thermal.ThermalGovernor:current_temperature > 77000)\
+                & (trappy.pid_controller.PIDController:output > 2500)"
         mask = parser.solve(eqn)
-        res = parser.ref(mask)
-        self.assertEquals(len(res), 67)
-        eqn = "(trappy.thermal.Thermal:temp > 69000) | (trappy.cpu_power.CpuOutPower:power < 800)"
-        mask = parser.solve(eqn)
-        res = parser.ref(~mask)
-        self.assertTrue(len(res), 17)
+        self.assertEquals(len(parser.ref(mask.dropna()[0])), 0)
 
     def test_bool_ops_scalar(self):
         """Test Logical Operations: Vector"""
 
+        thermal_zone_id=0
         parser = Parser(trappy.Run())
         # The equation returns a boolean scalar
         eqn = "(numpy.mean(trappy.thermal.Thermal:temp) > 65000) && (numpy.mean(trappy.cpu_power.CpuOutPower) > 500)"
-        self.assertTrue(parser.solve(eqn))
+        self.assertTrue(parser.solve(eqn)[thermal_zone_id])
         eqn = "(numpy.mean(trappy.thermal.Thermal:temp) > 65000) || (numpy.mean(trappy.cpu_power.CpuOutPower) < 500)"
-        self.assertTrue(parser.solve(eqn))
+        self.assertTrue(parser.solve(eqn)[thermal_zone_id])
 
     def test_single_func_call(self):
         """Test Single Function Call"""
 
+        thermal_zone_id = 0
         parser = Parser(trappy.Run())
         eqn = "numpy.mean(trappy.thermal.Thermal:temp)"
         self.assertEquals(
-            parser.solve(eqn),
+            parser.solve(eqn)[thermal_zone_id],
             np.mean(
                 parser.data.thermal.data_frame["temp"]))
 
@@ -114,31 +116,34 @@ trappy.thermal.Thermal:temp"
     def test_funcparams_mul(self):
         """Test Mult and Division: Data"""
 
+        thermal_zone_id = 0
         parser = Parser(trappy.Run())
         eqn = "trappy.thermal.Thermal:temp * 10.0"
         series = parser.data.thermal.data_frame["temp"]
-        assert_series_equal(parser.solve(eqn), series * 10.0)
+        assert_series_equal(parser.solve(eqn)[thermal_zone_id], series * 10.0)
         eqn = "trappy.thermal.Thermal:temp / trappy.thermal.Thermal:temp * 10"
-        assert_series_equal(parser.solve(eqn), series / series * 10)
+        assert_series_equal(parser.solve(eqn)[thermal_zone_id], series / series * 10)
 
     def test_var_forward(self):
         """Test Forwarding: Variable"""
 
+        thermal_zone_id = 0
         pvars = {}
         pvars["control_temp"] = 78000
         parser = Parser(trappy.Run(), pvars=pvars)
         eqn = "numpy.mean(trappy.thermal.Thermal:temp) < control_temp"
-        self.assertTrue(parser.solve(eqn))
+        self.assertTrue(parser.solve(eqn)[thermal_zone_id])
 
     def test_func_forward(self):
         """Test Forwarding: Mixed"""
 
+        thermal_zone_id = 0
         pvars = {}
         pvars["mean"] = np.mean
         pvars["control_temp"] = 78000
         parser = Parser(trappy.Run(), pvars=pvars)
         eqn = "mean(trappy.thermal.Thermal:temp) < control_temp"
-        self.assertTrue(parser.solve(eqn))
+        self.assertTrue(parser.solve(eqn)[thermal_zone_id])
 
     def test_cls_forward(self):
         """Test Forwarding: Classes"""
@@ -149,6 +154,7 @@ trappy.thermal.Thermal:temp"
         pvars["control_temp"] = 78000
         pvars["therm"] = cls
 
+        thermal_zone_id = 0
         parser = Parser(trappy.Run(), pvars=pvars)
         eqn = "mean(therm:temp) < control_temp"
-        self.assertTrue(parser.solve(eqn))
+        self.assertTrue(parser.solve(eqn)[thermal_zone_id])
