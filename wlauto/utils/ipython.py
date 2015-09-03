047308a904fa1aee514602f65be591f5c57588f9
Sergei Trofimov <sergei.trofimov@arm.com>
2015-09-03 12:07:41 +0100

    ipython: pylint fixes

--- wlauto/utils/ipython.py
+++ wlauto/utils/ipython.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+# pylint: disable=no-name-in-module,import-error,no-member
 
 import os
 import subprocess
