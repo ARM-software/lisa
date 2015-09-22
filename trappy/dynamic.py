ARM-software/trappy

doc: dynamic: Use sphinx friendly docstrings
Signed-off-by: Kapileshwar Singh <kapileshwar.singh@arm.com>

trappy/dynamic.py
@@ -26,7 +26,9 @@ from trappy.run import Run
 
 def default_init(self):
     """Default Constructor for the
-       Dynamic MetaClass
+    Dynamic MetaClass. This is used for
+    the dynamic object creation in
+    :mod:`trappy.dynamic.DynamicTypeFactory`
     """
 
     super(type(self), self).__init__(
@@ -38,7 +40,9 @@ def default_init(self):
 class DynamicTypeFactory(type):
 
     """Override the type class to create
-       a dynamic type on the fly
+    a dynamic type on the fly. This Factory
+    class is used internally by
+    :mod:`trappy.dynamic.register_dynamic`
     """
 
     def __new__(mcs, name, bases, dct):
@@ -52,7 +56,7 @@ class DynamicTypeFactory(type):
 
 def _get_name(name):
     """Internal Method to Change camelcase to
-       underscores. CamelCase -> camel_case
+    underscores. CamelCase -> camel_case
     """
     return re.sub('(?!^)([A-Z]+)', r'_\1', name).lower()
 
@@ -60,7 +64,40 @@ def _get_name(name):
 def register_dynamic(class_name, unique_word, scope="all",
                      parse_raw=False):
     """Create a Dynamic Type and register
-       it with the trappy Framework"""
+    it with the TRAPpy Framework
+
+    :param class_name: The name of the class to be registered
+        (Should be in CamelCase)
+    :type class_name: str
+
+    :param unique_word: The unique_word to be matched in the
+        trace
+    :type unique_word: str
+
+    :param scope: Registry Scope (Can be used to constrain
+        the parsing of events and group them together)
+    :type scope: str
+
+    :param parse_raw: If, true, raw trace output (-R flag)
+        will be used
+    :type parse_raw: bool
+
+    For example if a new unique word :code:`my_unique_word` has
+    to be registered with TRAPpy:
+    ::
+
+        import trappy
+        custom_class = trappy.register_dynamic("MyEvent", "my_unqiue_word")
+        run = trappy.Run("/path/to/trace_file")
+
+        # New data member created in the run object
+        run.my_event
+
+    .. note:: The name of the member is :code:`my_event` from **MyEvent**
+
+
+    :return: A class object of type :mod:`trappy.base.Base`
+    """
 
     dyn_class = DynamicTypeFactory(
         class_name, (Base,), {
@@ -76,9 +113,13 @@ def register_dynamic(class_name, unique_word, scope="all",
 
 def register_class(cls):
     """Register a new class implementation
-       Should be used when the class has
-       complex helper methods and does not
-       expect to use the default constructor
+    Should be used when the class has
+    complex helper methods and does not
+    expect to use the default constructor
+
+    :param cls: The class to be registered for
+        enabling the parsing of an event in trace
+    :type cls: :mod:`trappy.base.Base`
     """
 
     # Check the argspec of the class

ARM-software/lisa

trappy/dynamic.py
@@ -26,7 +26,9 @@ from trappy.run import Run
 
 def default_init(self):
     """Default Constructor for the
-       Dynamic MetaClass
+    Dynamic MetaClass. This is used for
+    the dynamic object creation in
+    :mod:`trappy.dynamic.DynamicTypeFactory`
     """
 
     super(type(self), self).__init__(
@@ -38,7 +40,9 @@ def default_init(self):
 class DynamicTypeFactory(type):
 
     """Override the type class to create
-       a dynamic type on the fly
+    a dynamic type on the fly. This Factory
+    class is used internally by
+    :mod:`trappy.dynamic.register_dynamic`
     """
 
     def __new__(mcs, name, bases, dct):
@@ -52,7 +56,7 @@ class DynamicTypeFactory(type):
 
 def _get_name(name):
     """Internal Method to Change camelcase to
-       underscores. CamelCase -> camel_case
+    underscores. CamelCase -> camel_case
     """
     return re.sub('(?!^)([A-Z]+)', r'_\1', name).lower()
 
@@ -60,7 +64,40 @@ def _get_name(name):
 def register_dynamic(class_name, unique_word, scope="all",
                      parse_raw=False):
     """Create a Dynamic Type and register
-       it with the trappy Framework"""
+    it with the TRAPpy Framework
+
+    :param class_name: The name of the class to be registered
+        (Should be in CamelCase)
+    :type class_name: str
+
+    :param unique_word: The unique_word to be matched in the
+        trace
+    :type unique_word: str
+
+    :param scope: Registry Scope (Can be used to constrain
+        the parsing of events and group them together)
+    :type scope: str
+
+    :param parse_raw: If, true, raw trace output (-R flag)
+        will be used
+    :type parse_raw: bool
+
+    For example if a new unique word :code:`my_unique_word` has
+    to be registered with TRAPpy:
+    ::
+
+        import trappy
+        custom_class = trappy.register_dynamic("MyEvent", "my_unique_word")
+        run = trappy.Run("/path/to/trace_file")
+
+        # New data member created in the run object
+        run.my_event
+
+    .. note:: The name of the member is :code:`my_event` from **MyEvent**
+
+
+    :return: A class object of type :mod:`trappy.base.Base`
+    """
 
     dyn_class = DynamicTypeFactory(
         class_name, (Base,), {
@@ -76,9 +113,13 @@ def register_dynamic(class_name, unique_word, scope="all",
 
 def register_class(cls):
     """Register a new class implementation
-       Should be used when the class has
-       complex helper methods and does not
-       expect to use the default constructor
+    Should be used when the class has
+    complex helper methods and does not
+    expect to use the default constructor
+
+    :param cls: The class to be registered for
+        enabling the parsing of an event in trace
+    :type cls: :mod:`trappy.base.Base`
     """
 
     # Check the argspec of the class