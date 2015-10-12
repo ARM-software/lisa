b0502b1bed4f3c4b0760740073abeec98446263b
Patrick Bellasi <patrick.bellasi@arm.com>
2015-10-12 15:34:06 +0100

    Update initial setup
    Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>

--- run_me_once.sh
+++ run_me_once.sh
@@ -10,6 +10,9 @@ echo "Initializing dependency libraries..."
 git submodule init
 git submodule update
 
+echo
+echo
+echo
 echo "To run the suite, first edit you configuration:"
 echo "  target.config        : to setup your target board"
 echo "  tests/eas/rfc.config : to define the experiment to run"
@@ -19,5 +22,6 @@ echo "  nosetests -v tests/eas/rfc.py"
 echo
 echo "Once tests have completed, you could report results by running"
 echo "./tools/report.py"
+echo
 echo
 
