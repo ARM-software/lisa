f97104f5c525f13f8375625062b0b7311a516d59
Patrick Bellasi <patrick.bellasi@arm.com>
2015-10-15 12:03:07 +0100

    utils/report: add support to report default wtype workloads
    Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>

--- libs/utils/report.py
+++ libs/utils/report.py
@@ -56,6 +56,7 @@ class Report(object):
 
         # Report all supported workload classes
         self.__rtapp_report(numbers)
+        self.__default_report(numbers)
 
     ############################### REPORT RTAPP ###############################
 
@@ -177,5 +178,147 @@ class Report(object):
                 res_line += ' {0:s}'.format(TestColors.rate(speedup_pct))
         res_line += ' |'
         print res_line
+
+    ############################### REPORT DEFAULT #############################
+
+    def __default_report(self, numbers):
+
+        # Build list of workload types which can be rendered using the default parser
+        wtypes = []
+        for supported_wtype in DEFAULT_WTYPES:
+            if supported_wtype in self.results.keys():
+                wtypes.append(supported_wtype)
+
+        if len(wtypes) == 0:
+            logging.debug('%14s - No Default workloads to report', 'ReportDefault')
+            return
+
+        logging.debug('%14s - Reporting Default workloads', 'ReportDefault')
+
+        # Setup lables depending on requested report
+        if numbers:
+            nrg_lable = 'Energy Indexes (Absolute)'
+            prf_lable = 'Performance Indexes (Absolute)'
+            logging.info('')
+            logging.info('%14s - Absolute comparisions:', 'Report')
+            print ''
+        else:
+            nrg_lable = 'Energy Indexes (Relative)'
+            prf_lable = 'Performance Indexes (Relative)'
+            logging.info('')
+            logging.info('%14s - Relative comparisions:', 'Report')
+            print ''
+
+        # Dump headers
+        print '{:9s}   {:15s} |'\
+                ' {:33s} | {:54s} |'\
+                .format('Test Id', 'Comparision',
+                        nrg_lable, prf_lable)
+        print '{:9s}   {:15s} |'\
+                ' {:>10s} {:>10s} {:>10s}  |'\
+                ' {:>10s} {:>10s} {:>10s} {:>10s} {:>10s} |'\
+                .format('', '',
+                        'LITTLE', 'big', 'Total',
+                        'Perf', 'CTime', 'EDP1', 'EDP2', 'EDP3')
+
+        # For each default test
+        for wtype in wtypes:
+            _results = self.results[wtype]
+            for tid in sorted(_results.keys()):
+                new_test = True
+                # For each configuration...
+                for i, base_idx in enumerate(sorted(_results[tid].keys())):
+                    # Which matches at least on base regexp
+                    for (base_rexp, test_rexp) in self.compare:
+                        if not base_rexp.match(base_idx):
+                            continue
+                        # Look for a configuration which matches the test regexp
+                        for test_idx in sorted(_results[tid].keys())[i+1:]:
+                            if test_idx == base_idx:
+                                continue
+                            if new_test:
+                                print '{:-<28s}+{:-<35s}+{:-<56s}+'\
+                                        .format('','', '')
+                                new_test = False
+                            if not test_rexp.match(test_idx):
+                                continue
+                            self.__default_compare(wtype, tid, base_idx, test_idx, numbers)
+
+        print ''
+
+    def __default_compare(self, wtype, tid, base_idx, test_idx, numbers):
+        _results = self.results[wtype]
+
+        logging.debug('Test %s: compare %s with %s',
+                tid, base_idx, test_idx)
+        res_comp = '{0:s} vs {1:s}'.format(test_idx, base_idx)
+        res_line = '{0:8s}: {1:15s} | '.format(tid, res_comp)
+
+        # Dump all energy metrics
+        for cpus in ['LITTLE', 'big', 'Total']:
+
+            # If either base of test have a 0 MAX energy, this measn that
+            # energy has not been collected
+            base_max = _results[tid][base_idx]['energy'][cpus]['max']
+            test_max = _results[tid][test_idx]['energy'][cpus]['max']
+            if base_max == 0 or test_max == 0:
+                res_line += ' {0:10s}'.format('NA')
+                continue
+
+            # Otherwise, report energy values
+            res_base = _results[tid][base_idx]['energy'][cpus]['avg']
+            res_test = _results[tid][test_idx]['energy'][cpus]['avg']
+
+            speedup_cnt =  res_test - res_base
+            if numbers:
+                res_line += ' {0:10.2f}'.format(speedup_cnt)
+            else:
+                speedup_pct =  100.0 * speedup_cnt / res_base
+                res_line += ' {0:s}'\
+                        .format(TestColors.rate(
+                            speedup_pct,
+                            positive_is_good = False))
+        res_line += ' |'
+
+        # If available, dump also performance results
+        if 'performance' not in _results[tid][base_idx].keys():
+            print res_line
+            return
+
+        for pidx in ['perf_avg', 'ctime_avg', 'edp1', 'edp2', 'edp3']:
+            res_base = _results[tid][base_idx]['performance'][pidx]['avg']
+            res_test = _results[tid][test_idx]['performance'][pidx]['avg']
+
+            logging.debug('idx: %s, base: %s, test: %s',
+                    pidx, res_base, res_test)
+
+            # Compute difference base-vs-test
+            speedup_cnt = 0
+            if res_base != 0:
+                if pidx in ['perf_avg']:
+                    speedup_cnt =  res_test - res_base
+                else:
+                    speedup_cnt =  res_base - res_test
+
+            # Compute speedup if required
+            speedup_pct = 0
+            if numbers:
+                if 'edp' in pidx:
+                    res_line += ' {0:10.2e}'.format(speedup_cnt)
+                else:
+                    res_line += ' {0:10.2f}'.format(speedup_cnt)
+            else:
+                if res_base != 0:
+                    if pidx in ['perf_avg']:
+                        # speedup_pct =  100.0 * speedup_cnt / res_base
+                        speedup_pct =  speedup_cnt
+                    else:
+                        speedup_pct =  100.0 * speedup_cnt / res_base
+                res_line += ' {0:s}'.format(TestColors.rate(speedup_pct))
+        res_line += ' |'
+        print res_line
+
+# List of workload types which can be parsed using the default test parser
+DEFAULT_WTYPES = ['perf_bench_messaging', 'perf_bench_pipe']
 
 #vim :set tabstop=4 shiftwidth=4 expandtab
