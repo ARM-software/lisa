ARM-software/lisa

commit 1c146e3ce78de44889712b58f293a859b9df8321

trace_cmd: more robust event fields parsing
The default paraser can now handle spaces in field values (but not field
names).

wlauto/utils/trace_cmd.py
@@ -15,6 +15,7 @@
 
 import re
 import logging
+from itertools import chain
 
 from wlauto.utils.misc import isiterable
 from wlauto.utils.types import numeric
@@ -133,10 +134,12 @@ def default_body_parser(event, text):
     the value into a numeric type, and failing that, keep it as string.
 
     """
-    kv_pairs = [e.split('=', 1) for e in text.split()]
-    new_values = {k: try_convert_to_numeric(v)
-                  for (k, v) in kv_pairs}
-    event.fields.update(new_values)
+    parts = [e.rsplit(' ', 1) for e in text.strip().split('=')]
+    parts = [p.strip() for p in chain.from_iterable(parts)]
+    if not len(parts) % 2:
+        i = iter(parts)
+        for k, v in zip(i, i):
+            event.fields[k] = v
 
 
 def regex_body_parser(regex, flags=0):

commit 7546232c103693ab570a1b2e5738820683bb2670

trace_cmd: fix parser to oportunistically convert parsed values to ints
This was done previosly but was broken by commit

1c146e3ce78de44889712b58f293a859b9df8321

wlauto/utils/trace_cmd.py
@@ -139,6 +139,10 @@ def default_body_parser(event, text):
     if not len(parts) % 2:
         i = iter(parts)
         for k, v in zip(i, i):
+            try:
+                v = int(v)
+            except ValueError:
+                pass
             event.fields[k] = v
 
 