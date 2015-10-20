4f9ce0c6089aff45c248519f55cf4ba82851ce76
Patrick Bellasi <patrick.bellasi@arm.com>
2015-10-20 16:59:11 +0100

    toosl/report: update script to support command line defined params
    Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>

--- tools/report.py
+++ tools/report.py
@@ -4,6 +4,21 @@ import sys
 sys.path.insert(1, "./libs")
 from utils.report import Report
 
+import argparse
+
+parser = argparse.ArgumentParser(
+        description='EAS RFC Configuration Comparator.')
+parser.add_argument('--bases', type=str,
+        default='bases_',
+        help='Regexp of BASE configurations to compare with')
+parser.add_argument('--tests',  type=str,
+        default='tests_',
+        help='Regexp of TEST configurations to compare against')
+parser.add_argument('--results', type=str,
+        default='./results_latest',
+        help='Folder containing experimental results')
+
 if __name__ == "__main__":
-    Report('./results_latest')
+    args = parser.parse_args()
+    Report(args.results, compare=[(args.bases, args.tests)])
 
