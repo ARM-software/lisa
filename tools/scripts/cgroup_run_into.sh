a4f63ca7df9171a4def906688cd6e671277ec0a8
Patrick Bellasi <patrick.bellasi@arm.com>
2015-11-10 18:06:00 +0000

    tools/scripts/cgroup: update to support CGroups with multiple controllers
    If the same CGroup is created in multiple hierarchies (i.e. controllers)
    this patch ensure that the workload will be allocated in all the same
    CGroup for all controllers.
    
    The lookup for a CGroup is done based on the CGroup name. Starting from
    the CGroup mount point, all the folders which name matches the CGroup are
    selected. The task PID is moved into each of these selected folders.
    
    In case no CGroups matching the specified name can be found, than the script
    returns with an error.
    
    Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>

--- tools/scripts/cgroup_run_into.sh
+++ tools/scripts/cgroup_run_into.sh
@@ -1,12 +1,25 @@
 #!/bin/sh
 
-DST_GRP=${1}
+# Control groups mount point
+CGMOUNT=${CGMOUNT:-/sys/fs/cgroup}
+# The control group we want to run into
+CGP=${1}
+# The command to run
 CMD=${2}
 
-if [ -f $DST_GRP/cgroup.procs ]
-then
-  echo $$ > $DST_GRP/cgroup.procs
-  $CMD
-else
-  echo "WARNING: couldn't find $DST_GRP cgroup!"
+# Check if the required CGroup exists
+find $CGMOUNT -type d | grep $CGP &>/dev/null
+if [ $? -ne 0 ]; then
+  echo "ERROR: could not find any $CGP cgroup under $CGMOUNT"
+  exit 1
 fi
+
+find $CGMOUNT -type d | grep $CGP | \
+while read CGPATH; do
+    # Move this shell into that control group
+    echo $$ > $CGPATH/cgroup.procs
+    echo "Moving task into $CGPATH"
+done
+
+# Execute the command
+$CMD
