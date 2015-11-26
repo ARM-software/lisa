5706782346edf3398b5d301ad7f0a9a608fd62f2
Kapileshwar Singh <kapileshwar.singh@arm.com>
2015-11-26 12:15:24 +0000

    tests: Add base class and trace files for testing
    Using the trace and utils_tests from TRAPpy
    
    Signed-off-by: Kapileshwar Singh <kapileshwar.singh@arm.com>

---
+++ tests/utils_tests.py
@@ -0,0 +1,56 @@
+#    Copyright 2015-2015 ARM Limited
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
+import unittest
+import os
+import shutil
+import subprocess
+import tempfile
+
+TESTS_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
+
+
+class SetupDirectory(unittest.TestCase):
+
+    def __init__(self, files_to_copy, *args, **kwargs):
+        self.files_to_copy = files_to_copy
+        super(SetupDirectory, self).__init__(*args, **kwargs)
+
+    def setUp(self):
+        self.previous_dir = os.getcwd()
+
+        self.out_dir = tempfile.mkdtemp()
+        os.chdir(self.out_dir)
+
+        for src_fname, dst_fname in self.files_to_copy:
+            src_fname = os.path.join(TESTS_DIRECTORY, src_fname)
+            shutil.copy(src_fname, os.path.join(self.out_dir, dst_fname))
+
+    def tearDown(self):
+        os.chdir(self.previous_dir)
+        shutil.rmtree(self.out_dir)
+
+
+class TestBART(SetupDirectory):
+
+    def __init__(self, *args, **kwargs):
+        super(TestBART, self).__init__(
+            [
+                ("./trace.txt", "trace.txt"),
+                ("./trace.raw.txt", "trace.raw.txt")
+            ],
+            *args,
+            **kwargs)
