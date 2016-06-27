d0e43e76188f4ccc8945b11eb27add3f911da34d
Michele Di Giorgio <michele.digiorgio@arm.com>
2016-06-27 14:26:17 +0100

    libs/utils/trace: refactor setXTimeRange()
    When setting x axis time range it is necessary to take into account non
    normalized traces as well by setting the boundaries to the plot window.
    
    Signed-off-by: Michele Di Giorgio <michele.digiorgio@arm.com>

--- libs/utils/trace.py
+++ libs/utils/trace.py
@@ -128,14 +128,13 @@ class Trace(object):
                           trace_format)
         self.__computeTimeSpan()
 
-        # Minimum and Maximum x_time to use for all plots
-        self.x_min = 0
-        self.x_max = self.time_range
-
         # Reset x axis time range to full scale
-        t_min = self.window[0]
-        t_max = self.window[1]
-        self.setXTimeRange(t_min, t_max)
+        self.x_min = self.window[0]
+        if self.window[0] is None:
+            self.x_min = 0
+        self.x_max = self.window[1]
+        if self.window[1] is None:
+            self.x_max = self.time_range
 
         self.data_frame = TraceData()
         self._registerDataFrameGetters(self)
@@ -169,16 +168,37 @@ class Trace(object):
         :param t_max: upper bound
         :type t_max: int or float
         """
-        if t_min is None:
+        self.x_min = self.window[0]
+        if self.window[0] is None:
             self.x_min = 0
-        else:
-            self.x_min = t_min
-        if t_max is None:
+        self.x_max = self.window[1]
+        if self.window[1] is None:
             self.x_max = self.time_range
-        else:
-            self.x_max = t_max
+
+        if t_min is not None:
+            if t_min < self.x_min:
+                logging.warning('t_min out of range: '
+                                'capping to trace minimum %.6f [s]',
+                                self.x_min)
+            elif t_min > self.x_max:
+                raise ValueError('t_min out of range: '
+                                 'trace boundaries are (%.6f, %.6f) [s]',
+                                 self.x_min, self.x_max)
+            else:
+                self.x_min = t_min
+        if t_max is not None:
+            if t_max > self.x_max:
+                logging.warning('t_max out of range: '
+                                'capping to trace maximum %.6f [s]',
+                                self.x_max)
+            elif t_max < self.x_min:
+                raise ValueError('t_max out of range: '
+                                 'trace boundaries are (%.6f, %.6f) [s]',
+                                 self.window[0], self.x_max)
+            else:
+                self.x_max = t_max
         logging.info('Set plots time range to (%.6f, %.6f)[s]',
-                     self.x_min, self.x_max)
+                self.x_min, self.x_max)
 
     def __registerTraceEvents(self, events):
         """
