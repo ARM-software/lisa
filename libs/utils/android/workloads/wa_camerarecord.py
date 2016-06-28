e912ff458925479ba733561dc0a8129b8972bafc
Michele Di Giorgio <michele.digiorgio@arm.com>
2016-06-28 16:08:16 +0100

    libs/utils/android/workloads: add WA-wrapped camera record workload
    Signed-off-by: Michele Di Giorgio <michele.digiorgio@arm.com>

---
+++ libs/utils/android/workloads/wa_camerarecord.py
@@ -0,0 +1,78 @@
+# SPDX-License-Identifier: Apache-2.0
+#
+# Copyright (C) 2015, ARM Limited and contributors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from android import Wlauto, Workload
+
+import os
+import glob
+import logging
+
+from shutil import copyfile, rmtree
+
+class WaCameraRecord(Wlauto):
+    """
+    Camera Record workload
+    """
+
+    # Package required by this workload
+    package = 'com.google.android.GoogleCamera'
+
+    # WA name of the workload
+    workload_name = 'camerarecord'
+
+
+    def __init__(self, test_env):
+        super(WaCameraRecord, self).__init__(test_env)
+        self._log = logging.getLogger('Camera Record')
+        self._log.debug('Workload created')
+
+    def run(self, exp_dir, agenda, collect=''):
+        """
+        :param exp_dir: Path to experiment directory where to store results.
+        :type exp_dir: str
+
+        :param agenda: Agenda to be passed to workload-automation. Can be a
+            YAML file or a dictionary.
+        :type agenda: str or dict
+
+        :param collect: Specifies what to collect. Possible values:
+            - 'energy'
+        :type collect: list(str)
+        """
+        # Setting agenda_yaml properly
+        _agenda = agenda
+        if isinstance(agenda, dict):
+            # Convert dictionary to YAML file
+            _agenda = self.generate_yaml_agenda(agenda)
+
+        self._log.debug('Running')
+
+        nrg_report = Wlauto.wa_run(exp_dir, _agenda,
+                                   WaCameraRecord.workload_name, collect=collect)
+
+        # Return frame stats file
+        db_file = os.path.join(exp_dir, "framestats.txt")
+
+        return db_file, nrg_report
+
+    @staticmethod
+    def _is_available(target):
+        if Wlauto._wload_is_available(WaCameraRecord.workload_name):
+            return True
+        return False
+
+# vim :set tabstop=4 shiftwidth=4 expandtab
