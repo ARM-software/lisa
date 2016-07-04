ARM-software/trappy

ILinePlot: learn to specify custom colors
The static plotter accepts a colors parameter to change the colors of
the lines.  Let the ILinePlot accept the same argument with the same
syntax so that we can customize the colors of the plots in the same way
for all plots created by trappy.plotter.

tests/test_plotter.py
@@ -276,6 +276,18 @@ class TestILinePlotter(unittest.TestCase):
         expected_index.sort()
         self.assertEquals(expected_index, merged.index.tolist())
 
+    def test_dygraph_colors(self):
+        """Check that to_dygraph_colors() constructs a valid dygraph colors argument"""
+        from trappy.plotter.ILinePlotGen import to_dygraph_colors
+
+        color_map = [[86, 58, 206]]
+        expected = '["rgb(86, 58, 206)"]'
+        self.assertEquals(to_dygraph_colors(color_map), expected)
+
+        color_map = [[0, 0, 0], [123, 23, 45]]
+        expected = '["rgb(0, 0, 0)", "rgb(123, 23, 45)"]'
+        self.assertEquals(to_dygraph_colors(color_map), expected)
+
 class TestBarPlot(unittest.TestCase):
     def setUp(self):
         self.dfr = pd.DataFrame({"foo": [1, 2, 3],

ARM-software/lisa

tests/test_plotter.py
@@ -276,6 +276,18 @@ class TestILinePlotter(unittest.TestCase):
         expected_index.sort()
         self.assertEquals(expected_index, merged.index.tolist())
 
+    def test_dygraph_colors(self):
+        """Check that to_dygraph_colors() constructs a valid dygraph colors argument"""
+        from trappy.plotter.ColorMap import to_dygraph_colors
+
+        color_map = [[86, 58, 206]]
+        expected = '["rgb(86, 58, 206)"]'
+        self.assertEquals(to_dygraph_colors(color_map), expected)
+
+        color_map = [[0, 0, 0], [123, 23, 45]]
+        expected = '["rgb(0, 0, 0)", "rgb(123, 23, 45)"]'
+        self.assertEquals(to_dygraph_colors(color_map), expected)
+
 class TestBarPlot(unittest.TestCase):
     def setUp(self):
         self.dfr = pd.DataFrame({"foo": [1, 2, 3],