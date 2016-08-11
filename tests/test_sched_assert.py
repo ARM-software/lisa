956b609efec4c4823fa4a5c353eef0feba8a30c7
Javi Merino <javi.merino@arm.com>
2016-08-11 17:54:23 +0100

    sched: SchedMultiAssert: add getCPUBusyTime
    For multiple tasks, it's valuable to verify that they have ran on a set
    of cpus for a given time.  With SchedMultiAssert.getCPUBusyTime() we can
    do that now!

--- tests/test_sched_assert.py
+++ tests/test_sched_assert.py
@@ -14,13 +14,14 @@
 #
 
 
+from bart.sched.SchedAssert import SchedAssert
+from bart.sched.SchedMultiAssert import SchedMultiAssert
 import trappy
 from trappy.stats.Topology import Topology
 import unittest
 
 import utils_tests
 
-from bart.sched.SchedAssert import SchedAssert
 
 @unittest.skipUnless(utils_tests.trace_cmd_installed(),
                      "trace-cmd not installed")
@@ -68,3 +69,41 @@ class TestSchedAssert(utils_tests.SetupDirectory):
         expected_time = 0.000817
         self.assertAlmostEqual(s.getRuntime(window=window), expected_time,
                                places=9)
+
+class TestSchedMultiAssert(utils_tests.SetupDirectory):
+    def __init__(self, *args, **kwargs):
+        self.big = [1,2]
+        self.little = [0, 3, 4, 5]
+        self.clusters = [self.big, self.little]
+        self.all_cpus = sorted(self.big + self.little)
+        self.topology = Topology(clusters=self.clusters)
+        super(TestSchedMultiAssert, self).__init__(
+             [("raw_trace.dat", "trace.dat")],
+             *args,
+             **kwargs)
+
+    def test_cpu_busy_time(self):
+        """SchedMultiAssert.getCPUBusyTime() work"""
+
+        # precalculated values against these processes in the trace
+        pids = [4729, 4734]
+        first_time = .000214
+        last_time = .003171
+
+        tr = trappy.FTrace()
+        sma = SchedMultiAssert(tr, self.topology, pids=pids)
+
+        expected_busy_time = 0.0041839999754810708
+        busy_time = sma.getCPUBusyTime("all", self.all_cpus, window=(first_time, last_time))
+        self.assertAlmostEqual(busy_time, expected_busy_time)
+
+        # percent calculation
+        expected_busy_pct = 23.582459561949445
+        busy_pct= sma.getCPUBusyTime("all", self.all_cpus, percent=True,
+                                     window=(first_time, last_time))
+        self.assertAlmostEqual(busy_pct, expected_busy_pct)
+
+        # percent without a window
+        expected_busy_pct = 23.018818156540004
+        busy_pct= sma.getCPUBusyTime("cluster", self.little, percent=True)
+        self.assertAlmostEqual(busy_pct, expected_busy_pct)
