43a28826460dd192805df1d57128d34f19c3ef80
Brendan Jackman <brendan.jackman@arm.com>
2016-09-22 19:08:51 +0100

    libs/utils/executor: Allow "profile" rt-app wloads to specify a "tasks" param
    On "periodic" rt-app workloads, the "tasks" param specifies how many
    instances of the given task configuration should be instantiated. For
    example, if the tests_conf contains:
    
        "wloads" : {
            "my_periodic" : {
                "type" : "rt-app",
                "conf" : {
                    "class" : "periodic",
                    "params" : {
                        "duty_cycle_pct": 100,
                        "duration_s" : 5,
                        "period_ms": 10
                    },
                    "tasks" : 10              // <-- Create 10 tasks
                },
            }
    
    then 10 tasks with the given "params" will be created.
    
    This commit adds the same functionality for tasks within workloads with
    a "class" of "profile". For example:
    
        "fmig" : {
            "type" : "rt-app",
            "conf" : {
                "class" : "profile",
                "params" : {
                    "small" : {
                        "kind" : "Periodic",
                        "params" : {
                            "duty_cycle_pct": 10,
                            "duration_s": 5,
                            "period_ms": 10,
                        },
                        "tasks" : 2           // <-- Create 2 10% tasks
                    },
                    "big" : {
                        "kind" : "Periodic",
                        "params" : {
                            "duty_cycle_pct": 100,
                            "duration_s" : 5,
                            "period_ms": 10
                        },
                        "tasks" : 4           // <-- and 4 100% tasks
                    },
                },
            },
        },
    
    The above will create a workload that runs 4 100% tasks and 2 10%
    periodic tasks in parallel.
    
    If the "tasks" field is not present, the value defaults to 1.

--- libs/utils/executor.py
+++ libs/utils/executor.py
@@ -381,9 +381,7 @@ class Executor():
         if conf['class'] == 'profile':
             params = {}
             # Load each task specification
-            for task_name in conf['params']:
-                task = conf['params'][task_name]
-                task_name = conf['prefix'] + task_name
+            for task_name, task in conf['params'].items():
                 if task['kind'] not in wlgen.__dict__:
                     logging.error(r'%14s - RTA task of kind [%s] not supported',
                             'RTApp', task['kind'])
@@ -392,7 +390,13 @@ class Executor():
                         'in RT-App workload specification'\
                         .format(task))
                 task_ctor = getattr(wlgen, task['kind'])
-                params[task_name] = task_ctor(**task['params']).get()
+                num_tasks = task.get('tasks', 1)
+                task_idxs = self._wload_task_idxs(wl_idx, num_tasks)
+                for idx in task_idxs:
+                    idx_name = str(idx) if len(task_idxs) > 0 else ""
+                    task_name_idx = conf['prefix'] + task_name + idx_name
+                    params[task_name_idx] = task_ctor(**task['params']).get()
+
             rtapp = wlgen.RTA(self.target,
                         wl_idx, calibration = self.te.calibration())
             rtapp.conf(kind='profile', params=params, loadref=loadref,
