f26af3fcfaaba9fc39e001a037a72c8028ec7e5f
Brendan Jackman <brendan.jackman@arm.com>
2016-11-09 18:34:31 +0000

    tests/eas: Add test for frequency invariant load tracking
    This test runs the same workload on the most capable CPU on the system
    at a cross section of available frequencies. The trace is then examined
    to find the average activation length of the workload, which is combined
    with the known period and frequency to estimate an expected mean value
    for util_avg for each frequency. The util_avg value is extract from
    scheduler trace events and its mean is compared with the expected
    value (ignoring the first 300ms so that the signal can stabilize). The
    test fails if the observed mean is beyond a certain error margin from
    the expected one. load_avg is then similarly compared with the expected
    util_avg mean, under the assumption that load_avg should equal util_avg
    when system load is light.
    
    The test is many helper methods, so that in the case of test failures
    the logic used by the test can be re-used to examine the _cause_ of the
    failure. A canned notebook is provided with basic plots showing test
    results in a more useful format than the command-line test run.

---
+++ tests/eas/load_tracking.py
@@ -0,0 +1,226 @@
+# SPDX-License-Identifier: Apache-2.0
+#
+# Copyright (C) 2016, ARM Limited and contributors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from bart.common.Utils import select_window, area_under_curve
+from devlib.utils.misc import memoized
+from trappy.stats.grammar import Parser
+import pandas as pd
+
+from test import LisaTest, experiment_test
+
+UTIL_SCALE = 1024
+# Time in seconds to allow for util_avg to converge (i.e. ignored time)
+UTIL_AVG_CONVERGENCE_TIME = 0.3
+# Allowed margin between expected and observed util_avg value
+ERROR_MARGIN_PCT = 15
+
+class FreqInvarianceTest(LisaTest):
+    """
+    Goal
+    ====
+    Basic check for frequency invariant load tracking
+
+    Detailed Description
+    ====================
+    This test runs the same workload on the most capable CPU on the system at a
+    cross section of available frequencies. The trace is then examined to find
+    the average activation length of the workload, which is combined with the
+    known period to estimate an expected mean value for util_avg for each
+    frequency. The util_avg value is extracted from scheduler trace events and
+    its mean is compared with the expected value (ignoring the first 300ms so
+    that the signal can stabilize). The test fails if the observed mean is
+    beyond a certain error margin from the expected one. load_avg is then
+    similarly compared with the expected util_avg mean, under the assumption
+    that load_avg should equal util_avg when system load is light.
+
+    Expected Behaviour
+    ==================
+    Load tracking signals are scaled so that the workload results in roughly the
+    same util & load values regardless of frequency.
+    """
+
+    test_conf = {
+        'tools'    : [ 'rt-app' ],
+        'ftrace' : {
+            'events' : [
+                'sched_switch',
+                'sched_load_avg_task',
+                'sched_load_avg_cpu',
+                'sched_pelt_se',
+            ],
+        },
+        # cgroups required by freeze_userspace flag
+        'modules': ['cpufreq', 'cgroups'],
+    }
+
+    @classmethod
+    def setUpClass(cls, *args, **kwargs):
+        super(FreqInvarianceTest, cls).runExperiments(*args, **kwargs)
+
+    @memoized
+    @classmethod
+    def _get_cpu(cls, target):
+        # Run on a 'big' CPU, or any CPU if not big.LITTLE
+        if hasattr(target, 'bl'):
+            return target.bl.bigs[0]
+        else:
+            return 0
+
+    @classmethod
+    def _getExperimentsConf(cls, test_env):
+        cpu = cls._get_cpu(test_env.target)
+
+        # 10% periodic RTApp workload:
+        wloads = {
+            'fie_10pct' : {
+                'type' : 'rt-app',
+                'conf' : {
+                    'class' : 'periodic',
+                    'params' : {
+                        'duty_cycle_pct': 10,
+                        'duration_s': 1,
+                        'period_ms': 16,
+                    },
+                    'tasks' : 1,
+                    'prefix' : 'fie_test',
+                    'cpus' : [cpu]
+                },
+            },
+        }
+
+        # Create a set of confs with different frequencies
+        # We'll run the 10% workload under each conf (i.e. at each frequency)
+        confs = []
+
+        all_freqs = test_env.target.cpufreq.list_frequencies(cpu)
+        # If we have loads of frequencies just test a cross-section so it
+        # doesn't take all day
+        cls.freqs = all_freqs[::len(all_freqs)/8 + 1]
+        for freq in cls.freqs:
+            confs.append({
+                'tag' : 'freq_{}'.format(freq),
+                'flags' : ['ftrace', 'freeze_userspace'],
+                'cpufreq' : {
+                    'freqs' : {cpu: freq},
+                    'governor' : 'userspace',
+                },
+            })
+
+        return {
+            'wloads': wloads,
+            'confs': confs,
+        }
+
+    def get_expected_util_avg(self, experiment):
+        """
+        Examine trace to figure out an expected mean for util_avg
+
+        Assumes an RT-App workload with a single task with a single phase,
+        running on a CPU with the highest max capacity in the system
+
+        This takes into account the frequency the workload was run at, but
+        doesn't use the kernel's data for compute capacities at each frequency,
+        instead it assumes that these values scale linearly.
+        """
+        [task] = experiment.wload.tasks.keys()
+        sched_assert = self.get_sched_assert(experiment, task)
+
+        [freq] = experiment.conf['cpufreq']['freqs'].values()
+        freq_scaling_factor = float(freq) / max(self.freqs)
+        duty_cycle_pct = sched_assert.getDutyCycle(self.get_window(experiment))
+
+        return UTIL_SCALE * (duty_cycle_pct / 100.) * freq_scaling_factor
+
+    def get_sched_task_signals(self, experiment, signals):
+        """
+        Get a pandas.DataFrame with the sched signals for the workload task
+
+        This examines scheduler load tracking trace events, supporting either
+        sched_load_avg_task or sched_pelt_se. You will need a target kernel that
+        includes these events.
+
+        :param experiment: Experiment to get trace for
+        :param signals: List of load tracking signals to extract. Probably a
+                        subset of ``['util_avg', 'load_avg']``
+        :returns: :class:`pandas.DataFrame` with a column for each signal for
+                  the experiment's workload task
+        """
+        [task] = experiment.wload.tasks.keys()
+        trace = self.get_trace(experiment)
+
+        # There are two different scheduler trace events that expose the load
+        # tracking signals. Neither of them is in mainline. Eventually they
+        # should be unified but for now we'll just check for both types of
+        # event.
+        # TODO: Add support for this parsing in Trappy and/or tasks_analysis
+        if 'sched_load_avg_task' in trace.available_events:
+            event = 'sched_load_avg_task'
+        elif 'sched_pelt_se' in trace.available_events:
+            event = 'sched_pelt_se'
+        else:
+            raise ValueError('No sched_load_avg_task or sched_pelt_se events. '
+                             'Does the kernel support them?')
+
+        df = getattr(trace.ftrace, event).data_frame
+        signals = df[df['comm'] == task][signals]
+        return select_window(signals, self.get_window(experiment))
+
+    def get_signal_mean(self, experiment, signal,
+                        ignore_first_s=UTIL_AVG_CONVERGENCE_TIME):
+        """
+        Get the mean of a scheduler signal for the experiment's task
+
+        Ignore the first `ignore_first_s` seconds of the signal.
+        """
+        (wload_start, wload_end) = self.get_window(experiment)
+        window = (wload_start + ignore_first_s, wload_end)
+
+        signal = self.get_sched_task_signals(experiment, [signal])[signal]
+        signal = select_window(signal, window)
+        return area_under_curve(signal) / (window[1] - window[0])
+
+    def _test_signal(self, experiment, tasks, signal_name):
+        [task] = tasks
+        exp_util = self.get_expected_util_avg(experiment)
+        signal_mean = self.get_signal_mean(experiment, signal_name)
+
+        error_margin = exp_util * (ERROR_MARGIN_PCT / 100.)
+        [freq] = experiment.conf['cpufreq']['freqs'].values()
+
+        msg = 'Saw {} around {}, expected {} at freq {}'.format(
+            signal_name, signal_mean, exp_util, freq)
+        self.assertAlmostEqual(signal_mean, exp_util, delta=error_margin,
+                               msg=msg)
+
+    @experiment_test
+    def test_task_util_avg(self, experiment, tasks):
+        """
+        Test that the mean of the util_avg signal matched the expected value
+        """
+        return self._test_signal(experiment, tasks, 'util_avg')
+
+    @experiment_test
+    def test_task_load_avg(self, experiment, tasks):
+        """
+        Test that the mean of the load_avg signal matched the expected value
+
+        Assuming that the system was under little stress (so the task was
+        RUNNING whenever it was RUNNABLE) and that the task was run with a
+        'nice' value of 0, the load_avg should be similar to the util_avg. So,
+        this test does the same as test_task_util_avg but for load_avg.
+        """
+        return self._test_signal(experiment, tasks, 'load_avg')
