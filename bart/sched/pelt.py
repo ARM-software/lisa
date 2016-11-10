7296c5aad5be3abc7926a2ca2f12046963f5ff18
Patrick Bellasi <patrick.bellasi@arm.com>
2016-11-10 18:27:39 +0000

    sched: add first simple PELT simulator
    This patch provide a first implementation of a simple PELT simulator.
    Despite being of a bare simplicity it's still a useful tool to show and
    study how PELT describes a task depending on its profile (period and
    duty-cycle) as well as some specific PELT attributes (half-life, initial
    values).
    The current PELT model can be easily extended to evaluate experimental
    features such as (for example) the capping of the decay value.
    
    Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>
    Reviewed-by: KP Singh <kpsingh@google.com>

---
+++ bart/sched/pelt.py
@@ -0,0 +1,464 @@
+#    Copyright 2015-2016 ARM Limited
+#    Copyright 2016 Google Inc. All Rights Reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+"""Simple PELT Simulator
+
+This module provides a simple yet useful simulator of the Per-Entity Load
+Tracking (PELT) machinery used by the Linux kernel to track the CPU bandwidth
+demand of scheduling entities.
+
+This model is based on some simplification assumptions:
+1) it supports only periodic tasks, which have a constant period and duty-cycle
+2) all the time metrics are defined using "PELT samples" (1024us by default) as
+   fundamental unit of measure
+
+The simulator is composed of two main components: a PeriodicTask model and a signal
+Simulator. Both classes have configuration parameters to defined their specific
+behavior. Specifically, the Simulator requires also the PeriodicTask which signal
+has to be computed.
+"""
+
+import math
+
+from collections import namedtuple as namedtuple
+from pandas import DataFrame as DataFrame
+
+
+################################################################################
+# PeriodicTask Model
+################################################################################
+
+class PeriodicTask(object):
+
+    def __init__(self, period_samples, start_sample=0,
+                 run_samples=None, duty_cycle_pct=None,
+                 pelt_sample_us=1024, pelt_max=1024):
+        """A simulated periodic task
+
+        Task activations are specified in terms of PELT samples.
+        A [PELT sample] is by default 1024 us long, this is the time interval
+        used by the Linux scheduler to update a PELT signal.
+
+        :param start_sample: task start time [pelt_sample_us]
+        :type start_sample: int
+
+        :param period_samples: the period of each activation [pelt_sample_us]
+        :type period_samples: int
+
+        :param run_samples: the duration of each activation [pelt_sample_us]
+                            This parameter is mutually exclusive with
+                            duty_cycle_pct
+        :type run_samples: int
+
+        :param duty_cycle_pct: the duty-cycle of each activation
+                               This parameter is mutually exclusive with the
+                               run_samples
+        :type duty_cycle_pct: int
+
+        :param pelt_sample_us: the duration of a PELT sample, 1024us by default
+        :type pelt_sample_us: int
+
+        :param pelt_max: the maximum value of the PELT signal, 1024 by default
+        :type pelt_max: int
+        """
+
+        args = [period_samples, start_sample, pelt_sample_us, pelt_max]
+        invalid = any([True for param in args if
+                       not isinstance(param, int) or param < 0])
+        if invalid:
+            raise ValueError('one of more parameters are not positive integers')
+        if run_samples is None and duty_cycle_pct is None:
+            raise ValueError('undefined run_samples or duty_cycle_pct')
+        if period_samples == 0:
+            raise ValueError('period_samples cannot be 0')
+        if run_samples and run_samples > period_samples:
+            raise ValueError('run_samples bigger than period_samples')
+
+        self.pelt_sample_us = pelt_sample_us
+        self.pelt_max       = pelt_max
+
+        self.start_sample = start_sample
+        self.start_us  = self.pelt_sample_us * start_sample
+        self.period_samples = period_samples
+        self.period_us = self.pelt_sample_us * period_samples
+
+        if duty_cycle_pct:
+            self.run_samples = period_samples * float(duty_cycle_pct) / 100
+            self.duty_cycle_pct = duty_cycle_pct
+        else:
+            self.run_samples = run_samples
+            self.duty_cycle_pct = int(100. * run_samples / period_samples)
+        self.run_us    = self.pelt_sample_us * self.run_samples
+
+        self.idle_us   = self.period_us - self.run_us
+        self.pelt_avg =  int(float(self.pelt_max) * self.duty_cycle_pct / 100)
+
+    def isRunning(self, time_ms):
+        """Return wheter the task is running in the specified time instant.
+
+        The current implementation provide support only for periodic tasks
+        which have the following (abstract) execution model:
+
+            +----+    +----+      Task is RUNNING
+            |    |    |    |
+                 +----+    +----  Task is SLEEPING
+
+           -+----+----+----+-------------------------- Time --->
+            1    2    3    4      PELT Samples
+           ^    ^    ^    ^       Task running sampling time
+           R    S    R    S       Task Status reported by this function
+
+
+        Since we sample exactly at PELT samples intervals, the exact time when
+        a task starts to run we consider it as if if was running for the
+        entire previous PELT interval. To fix this, by exploting the knowledge
+        that a task is always configured using an integer number of run/speep
+        PELT samples, we can look at the 1us before time wrt the specified
+        sample time.
+        """
+        # The 1us "before time" is always represeting the exact status of the
+        # task in that period. This is why we remove 1 in the following
+        # computation:
+        period_delta_us = (float(time_ms) * 1e3 - 1) % self.period_us
+        if period_delta_us <= self.run_us:
+            return True
+        return False
+
+    def __str__(self):
+        return "PeriodicTask(start: {:.3f} [ms], period: {:.3f} [ms], run: {:.3f} [ms], "\
+               "duty_cycle: {:.3f} [%], pelt_avg: {:d})"\
+                .format(self.start_us/1e3, self.period_us/1e3,
+                        self.run_us/1e3, self.duty_cycle_pct,
+                        self.pelt_avg)
+
+
+################################################################################
+# PELT Model
+################################################################################
+
+_PELTStats = namedtuple('PELTStats', [
+    'start_s', 'end_s',
+    'pelt_avg', 'pelt_init', 'half_life',
+    'tmin', 'tmax', 'min', 'max', 'avg', 'std',
+    'err', 'err_pct'])
+class PELTStats(_PELTStats):
+    """Statistic of a computed PELT signal
+
+    This is a collection of metrics computed on a simulated PELT signal:
+    start_s   : start time [s] used for PELT signal simulation
+    end_s     : end time [s] used for PELT signal simulation
+    pelt_avg  : expected average value of the PELT signal
+    pelt_init : initial PELT value
+    halt_life : time [ms] of the PELT's halt-life parameter
+    tmin      : start time [s] used for statistics computation
+    tmax      : end time [s] used for statistics computation
+    min       : minimum PELT value, within [tmin, tmax]
+    max       : maximum PELT value, within [tmin, tmax]
+    avg       : average of the PELT values, within [tmin, tmax]
+    std       : standard deviation of the PELT value, within [tmin, tmax]
+    err       : difference between pelt_avg and avg
+    err_pct   : the avg error percentage, compared to the expected average (pelt_avg)
+
+    :note: the [start_s, end_s] interval used for statistical computations can be
+           smaller than the timespan of the simulated PETL signal. For example, we
+           can simulate a PELT signal for 3 [s] but compute statistics only for the
+           last 1 [s]. This is allows for example to ignore the initial execution
+           of a task while focusing for stats only on a timeframe in which the
+           signal can be considered stable.
+    """
+    pass
+
+_PELTRange = namedtuple('PELTRange', [
+    'min_value', 'max_value'])
+class PELTRange(_PELTRange):
+    """Stability range for the PELT signal of a given PeriodicTask
+
+    Given a PeriodicTask and a specific PELT configuration the simulated PELT
+    signal is expected to stabilize in the range [min_value, max_value].
+    """
+    pass
+
+class Simulator(object):
+    """A simple PELT simulator
+
+    This PELT simulator allows to compute and plot the PELT signal for a
+    specifed (periodic) task. The simulator can be configured using the main
+    settings exposed by PELT: the half-life of a task and its intial value.
+
+    The model is pretty simple and works by generating a new value at each
+    PELT sample interval (_sample_us) specified in [us] (1024 by default).
+
+    The model allows to compute statistics on the generated signal, over a
+    specified time frame. It provides also an embedded support to plot the
+    generated signal along with a set of expected thresholds and values
+    (e.g. half-life, min/max stability values)
+    """
+
+    _sample_us = 1024
+    _signal_max = 1024
+
+    def __init__(self, init_value=0, half_life_ms=32):
+        """Initialize the PELT simulator for a specified task.
+
+        :param init_value: the initial PELT value for the task
+        :type  init_value: int
+
+        :param half_life_ms: the [ms] interval required by a signal to
+                             increased/decreased by half of its range
+        :type  half_life_ms: int
+        """
+        self.init_value = init_value
+        self.half_life_ms = half_life_ms
+
+        self._geom_y = pow(0.5, 1./half_life_ms)
+        self._geom_u = float(Simulator._signal_max) * (1. - self._geom_y)
+
+        self.task = None
+        self._df = None
+
+    def __str__(self):
+        desc  = "PELT Simulator configured with\n"
+        desc += "  initial value        : {}\n".format(self.init_value)
+        desc += "  half life (HL)  [ms] : {}\n".format(self.half_life_ms)
+        desc += "  y =    0.5^(1/HL)    : {:6.3f}\n".format(self._geom_y)
+        desc += "  u = {:5d}*(1 - y)    : {:6.3f}\n".format(Simulator._signal_max, self._geom_u)
+        return desc
+
+    def _geomSum(self, u_1, active_us=0):
+        """Geometric add the specified value to the u_1 series.
+
+        NOTE: the current implementation assume that the task was active
+              (active_us != 0) or sleeping (active_us == 0) a full PELT
+              sampling interval (by default 1024[us])
+        """
+        # Decay previous samples
+        u_1 *= self._geom_y
+        # Add current sample (if task was active)
+        if active_us:
+            u_1 += self._geom_u
+        return u_1
+
+    def stableRange(self, task):
+        """Compute the PELT's signal stability ranges for the specified task
+
+        Here we use:
+        - Decay factor:       y = 0.5^(1/half_life)
+        - Max stable value:       (1-y^r) / (1-y^p)
+        - Min stable value: y^i * (1-y^r) / (1-y^p)
+        Where:
+            r is the run time of the task in number of PELT samples
+            p is the period of the task in number of PETL samples
+            i is the idle time of the task in number of PELT samples,
+              i.e. i = p - r
+
+        :param task: the task we want to simulate the PELT signal for, by
+                     default is the task used to initialize the Simulator
+                     (when specified)
+        :type  task: PeriodicTask
+
+        :return: :mod:`PELTRange` instance representing the minimum and maximum
+                 value of the PELT signal once stable.
+
+        """
+
+        # Validate input parameters
+        if not isinstance(task, PeriodicTask):
+            raise ValueError("Wrong time for task parameter")
+
+        def _to_pelt_samples(time_us):
+            return float(time_us)/Simulator._sample_us
+
+        # Compute max value
+        max_pelt  = (1. - pow(self._geom_y, _to_pelt_samples(task.run_us)))
+        max_pelt /= (1. - pow(self._geom_y, _to_pelt_samples(task.period_us)))
+
+        # Compute min value, by decaying the maximum value
+        min_pelt  = max_pelt
+        min_pelt *= pow(self._geom_y, _to_pelt_samples(task.idle_us))
+
+        return PELTRange(Simulator._signal_max * min_pelt,
+                         Simulator._signal_max * max_pelt)
+
+
+    def getSignal(self, task, start_s=0, end_s=10):
+        """Compute the PELT signal for the specified task and interval.
+
+        :param task: the task we want to simulate the PELT signal for, by
+                     default is the task used to intialize the Simulator
+                     (when specified)
+        :type  task: PeriodicTask
+
+        :param start_s: the start time in [s]
+        :type  start_s: float
+
+        :param end_s: the end time in [s]
+        :type  end_s: float
+
+        :return: :mod:`pandas.DataFrame` instance which reports the computed
+        PELT values at each PELT sample interval. The returned columns are:
+        - Time          : the PELT sample time
+        - PELT_Interval : the PELT sample number
+        - Running       : a boolean reporting if the task was RUNNING in that
+                          PELT interval
+        - pelt_value      : the compute PELT signal value at the end of that PELT
+                          interval
+        """
+
+        # Computed PELT samples
+        samples = []
+
+        # Validate input parameters
+        if not isinstance(task, PeriodicTask):
+            raise ValueError("Wrong type for task parameter")
+
+        # Intervals (in PELT samples) for the signal to compute
+        self.start_us = _s_to_us(start_s, Simulator._sample_us)
+        self.end_us = _s_to_us(end_s, Simulator._sample_us)
+
+        # Add initial value to the generated signal
+        pelt_value = self.init_value
+        t_us = self.start_us
+        sample = (_us_to_s(t_us), 0, False, pelt_value)
+        samples.append(sample)
+
+        # Compute following PELT samples
+        t_us = self.start_us + Simulator._sample_us
+        while t_us <= self.end_us:
+
+            # Check if the task was running in the current PELT sample
+            running = task.isRunning(_us_to_ms(t_us))
+
+            # Assume the task was running for all the current PELT sample
+            active_us = Simulator._sample_us if running else 0
+            pelt_value = self._geomSum(pelt_value, active_us)
+
+            # Append PELT sample
+            sample = (_us_to_s(t_us), t_us/Simulator._sample_us, running, pelt_value)
+            samples.append(sample)
+
+            # Prepare for next sample computation
+            t_us += Simulator._sample_us
+
+        # Create DataFrame from computed samples
+        self._df = DataFrame(samples, columns=['Time', 'PELT_Interval', 'Running', 'pelt_value'])
+        self._df.set_index('Time', inplace=True)
+
+        # Keep track of the last task we computed the signal for
+        self.task = task
+
+        return self._df
+
+    def getStats(self, stats_start_s=0, stats_end_s=None):
+        """Compute the stats over a pre-computed PELT signal, considering only
+        the specified portion of the signal.
+
+        :param stats_start_s: the start of signal portion to build the stats for
+        :type  stats_start_s: float
+
+        :param stats_end_s: the end of signal portion to build the stats for
+        :type  stats_end_s: float
+
+        :return: :mod:`PELTStats' instance reporting the metrics of the PELT
+                 signal computed for the task specified in the last execution
+                 of getSignal.
+        """
+
+        # Validate input parameters
+        if self._df is None:
+            raise ValueError("no signal computed, run getSignal before")
+
+        # Intervals (in PELT samples) for the statistics to compute
+        stats_start_s = max(stats_start_s, self._df.index.min())
+        if stats_end_s:
+            stats_end_s = min(stats_end_s, self._df.index.max())
+        else:
+            stats_end_s = self._df.index.max()
+
+        df = self._df[stats_start_s:stats_end_s]
+
+        # Compute stats
+        _pelt_avg = self.task.pelt_avg
+        _pelt_init = self.init_value
+        _half_life = self.half_life_ms
+        _tmin = stats_start_s
+        _tmax = stats_end_s
+        _min = df.pelt_value.min()
+        _max = df.pelt_value.max()
+        _avg = df.pelt_value.mean()
+        _std = df.pelt_value.std()
+        _err = _avg - self.task.pelt_avg
+        _err_pct = None
+        if self.task.pelt_avg:
+            _err_pct = 100 * _err / self.task.pelt_avg
+
+        return PELTStats(
+            _us_to_s(self.start_us), _us_to_s(self.end_us),
+            _pelt_avg, _pelt_init, _half_life,
+            _tmin, _tmax, _min, _max,
+            _avg, _std, _err, _err_pct)
+
+
+################################################################################
+# Utility Functions
+################################################################################
+
+def _s_to_us(time_s, interval_us=1e3):
+    """Convert [s] into the (not smaller) nearest [us]
+
+    Translate a time expressed in [s] to the nearest time in [us]
+    which is a integer multiple of the specified interval_us and it is not
+    smaller than the original time_s.
+
+    Example:
+        _s_to_us(1.0)       => 1000000 [us]
+        _s_to_us(1.0, 1024) => 1000448 [us]
+
+        _s_to_us(1.1)       => 1100000 [us]
+        _s_to_us(1.1, 1024) => 1100800 [us]
+    """
+    return interval_us * int(math.ceil((1e6 * time_s) / interval_us))
+
+def _ms_to_us(time_ms, interval_us=1e3):
+    """Convert [ms] into the (not smaller) nearest [us]
+
+    Translate a time expressed in [ms] to the nearest time in [us]
+    which is a integer multiple of the specified interval_us and it is not
+    smaller than the original time_s.
+
+    Example:
+        _ms_to_us(1.0)       => 1000 [us]
+        _ms_to_us(1.0, 1024) => 1024 [us]
+
+        _ms_to_us(1.1)       => 2000 [us]
+        _ms_to_us(1.1, 1024) => 2048 [us]
+    """
+    return interval_us * int(math.ceil((1e3 * time_ms) / interval_us))
+
+def _us_to_s(time_us):
+    """Convert [us] into (float) [s]
+    """
+    return (float(time_us)/1e6)
+
+def _us_to_ms(time_us):
+    """Convert [us] into (float) [ms]
+    """
+    return (float(time_us)/1e3)
+
+def _ms_to_s(time_ms):
+    """Convert [ms] into (float) [s]
+    """
+    return (float(time_ms)/1e3)
+
