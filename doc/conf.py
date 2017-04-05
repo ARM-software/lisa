d1ad79c59a8719b8ae995679b7ffe72630abbddb
Brendan Jackman <brendan.jackman@arm.com>
2017-04-05 11:46:22 +0100

    doc: Add libs to sys.path in sphinx's conf.py
    This means we can build the docs without sourcing init_env - this is useful
    for readthedocs.io where we don't get to specify arbitrary commands.

--- doc/conf.py
+++ doc/conf.py
@@ -19,6 +19,10 @@ import os
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
+lisa_home = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..')
+sys.path.insert(0, lisa_home)
+for lib in ['utils', 'wlgen', 'trappy', 'bart', 'devlib']:
+    sys.path.insert(0, os.path.join(lisa_home, 'libs', lib))
 
 # -- General configuration ------------------------------------------------
 
