c557fe13a747cab9b36a7c46b3325e8d5f292933
Brendan Jackman <brendan.jackman@arm.com>
2017-05-23 12:30:04 +0100

    Revert "no"
    This reverts commit 1b42dd9b97cdbc43bde56038714c28c3a6ec13cc.

--- doc/conf.py
+++ doc/conf.py
@@ -52,7 +52,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'LISA'
-copyright = u'2016, ARM-Software'
+copyright = u'2017, ARM-Software'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
