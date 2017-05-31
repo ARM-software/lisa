ef3b80d296671a1b11bef174f9abd08a430c450b
Marc Bonnici <marc.bonnici@arm.com>
2017-05-31 16:46:12 +0100

    Benchmarkpi: Updated to uiautomator2

--- wa/workloads/benchmarkpi/uiauto/build.sh
+++ wa/workloads/benchmarkpi/uiauto/build.sh
@@ -15,13 +15,41 @@
 #
 set -e
 
-class_dir=bin/classes/com/arm/wa/uiauto
-base_class=`python -c "import os, wa; print os.path.join(os.path.dirname(wa.__file__), 'framework', 'uiauto', 'BaseUiAutomation.class')"`
-mkdir -p $class_dir
-cp $base_class $class_dir
+# CD into build dir if possible - allows building from any directory
+script_path='.'
+if `readlink -f $0 &>/dev/null`; then
+    script_path=`readlink -f $0 2>/dev/null`
+fi
+script_dir=`dirname $script_path`
+cd $script_dir
 
-ant build
+# Ensure gradelw exists before starting
+if [[ ! -f gradlew ]]; then
+    echo 'gradlew file not found! Check that you are in the right directory.'
+    exit 9
+fi
 
-if [[ -f bin/com.arm.wa.uiauto.benchmarkpi.jar ]]; then
-    cp bin/com.arm.wa.uiauto.benchmarkpi.jar ..
+# Copy base class library from wlauto dist
+libs_dir=app/libs
+base_class=`python -c "import os, wa; print os.path.join(os.path.dirname(wa.__file__), 'framework', 'uiauto', 'uiauto.aar')"`
+mkdir -p $libs_dir
+cp $base_class $libs_dir
+
+# Build and return appropriate exit code if failed
+# gradle build
+./gradlew clean :app:assembleDebug
+exit_code=$?
+if [[ $exit_code -ne 0 ]]; then
+    echo "ERROR: 'gradle build' exited with code $exit_code"
+    exit $exit_code
+fi
+
+# If successful move APK file to workload folder (overwrite previous)
+package=com.arm.wa.uiauto.benchmarkpi
+rm -f ../$package
+if [[ -f app/build/apk/$package.apk ]]; then
+    cp app/build/apk/$package.apk ../$package.apk
+else
+    echo 'ERROR: UiAutomator apk could not be found!'
+    exit 9
 fi
