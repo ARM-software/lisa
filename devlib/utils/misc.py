ARM-software/lisa

commit 86c9b6a1c7ef593dacede2c622cbaaefcad6b0dd

utils/misc: update MIDR table
Update the MIDR CPU part number table with values from
https://github.com/torvalds/linux/blob/master/arch/arm64/include/asm/cputype.h

devlib/utils/misc.py
@@ -79,8 +79,17 @@ CPU_PART_MAP = {
         0xd08: {None: 'A72'},
         0xd09: {None: 'A73'},
     },
+    0x42: {  # Broadcom
+        0x516: {None: 'Vulcan'},
+    0x43: {  # Cavium
+        0x0a1: {None: 'Thunderx'},
+        0x0a2: {None: 'Thunderx81xx'},
+    },
     0x4e: {  # Nvidia
         0x0: {None: 'Denver'},
+    },
+    0x50: {  # AppliedMicro
+        0x0: {None: 'xgene'},
     },
     0x51: {  # Qualcomm
         0x02d: {None: 'Scorpion'},
@@ -91,6 +100,7 @@ CPU_PART_MAP = {
         },
         0x205: {0x1: 'KryoSilver'},
         0x211: {0x1: 'KryoGold'},
+        0x800: {None: 'Falkor'},
     },
     0x56: {  # Marvell
         0x131: {

commit 0a95bbed879f8f350325f2a2d3c7056f2a531955

misc.py: fix syntax error introduced by 86c9b6a1c7e
Signed-off-by: Ionela Voinescu <ionela.voinescu@arm.com>

devlib/utils/misc.py
@@ -81,6 +81,7 @@ CPU_PART_MAP = {
     },
     0x42: {  # Broadcom
         0x516: {None: 'Vulcan'},
+    },
     0x43: {  # Cavium
         0x0a1: {None: 'Thunderx'},
         0x0a2: {None: 'Thunderx81xx'},