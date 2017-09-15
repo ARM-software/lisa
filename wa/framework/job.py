1e233d21043fff03b942a8036d687228fc08cb80
Sergei Trofimov <sergei.trofimov@arm.com>
2017-09-15 13:44:20 +0100

    framework/job: track run time.
    Job now tracks how long it took to run the workload and save the
    timedelta value in the run_time attribute.

--- wa/framework/job.py
+++ wa/framework/job.py
@@ -1,4 +1,5 @@
 import logging
+from datetime import datetime
 
 from wa.framework import pluginloader, signal
 from wa.framework.configuration.core import Status
@@ -37,6 +38,7 @@ class Job(object):
         self.context = context
         self.workload = None
         self.output = None
+        self.run_time = None
         self.retries = 0
         self._status = Status.NEW
 
@@ -71,7 +73,11 @@ class Job(object):
     def run(self, context):
         self.logger.info('Running job {}'.format(self.id))
         with signal.wrap('WORKLOAD_EXECUTION', self, context):
-            self.workload.run(context)
+            start_time = datetime.utcnow()
+            try:
+                self.workload.run(context)
+            finally:
+                self.run_time = datetime.utcnow() - start_time
 
     def process_output(self, context):
         self.logger.info('Processing output for job {}'.format(self.id))
