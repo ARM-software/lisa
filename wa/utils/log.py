4b7d43de9e6412957b4d37e5aba60433a9bdb969
Brendan Jackman <brendan.jackman@arm.com>
2017-10-23 17:25:15 +0100

    utils/log: Convert exception to string before passing to Logger
    Now that we add an event when logging errors, we need to avoid passing an
    exception object directly to the logger. Otherwise we end up adding the
    unserializable Exception object to the events list, which causes an error when
    trying to write the output.

--- wa/utils/log.py
+++ wa/utils/log.py
@@ -171,7 +171,7 @@ def log_error(e, logger, critical=False):
     if isinstance(e, KeyboardInterrupt):
         log_func('Got CTRL-C. Aborting.')
     elif isinstance(e, WAError) or isinstance(e, DevlibError):
-        log_func(e)
+        log_func(str(e))
     elif isinstance(e, subprocess.CalledProcessError):
         tb = get_traceback()
         log_func(tb)
