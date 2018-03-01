a9329554d19f293856baecb270ba15c0a15a186f
Sergei Trofimov <sergei.trofimov@arm.com>
2018-03-01 14:10:20 +0000

    wa: fix exception exports

--- wa/__init__.py
+++ wa/__init__.py
@@ -2,12 +2,11 @@ from wa.framework import pluginloader, signal
 from wa.framework.command import Command, ComplexCommand, SubCommand
 from wa.framework.configuration import settings
 from wa.framework.configuration.core import Status
-from wa.framework.exception import HostError, JobError, InstrumentError, ConfigError
-from wa.framework.exception import (OutputProcessorError, ResourceError,
-                                    CommandError, ToolError)
-from wa.framework.exception import (WAError, NotFoundError, ValidationError,
-                                    WorkloadError)
-from wa.framework.exception import WorkerThreadError, PluginLoaderError
+from wa.framework.exception import (CommandError, ConfigError, HostError, InstrumentError,
+                                    JobError, NotFoundError, OutputProcessorError,
+                                    PluginLoaderError, ResourceError, TargetError,
+                                    TargetNotRespondingError, TimeoutError, ToolError,
+                                    ValidationError, WAError, WorkloadError, WorkerThreadError)
 from wa.framework.instrument import (Instrument, very_slow, slow, normal, fast,
                                      very_fast)
 from wa.framework.output import RunOutput, discover_wa_outputs
