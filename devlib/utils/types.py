9fd690efb303ecc5dfc80d2f5806bff5d8b0ee5d
Sergei Trofimov <sergei.trofimov@arm.com>
2018-07-04 15:52:22 +0100

    Update copyrights
    - Update the year in the copyrights to match the last year the file was
      modified.
    - Add the copyright header to files that did not already have one.

--- devlib/utils/types.py
+++ devlib/utils/types.py
@@ -1,4 +1,4 @@
-#    Copyright 2014-2015 ARM Limited
+#    Copyright 2014-2018 ARM Limited
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
