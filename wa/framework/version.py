d858435c3d81b9fc7c37e2917723a1486c1d5950
Marc Bonnici <marc.bonnici@arm.com>
2018-07-26 11:26:04 +0100

    utils/version: Fix check to only decode bytes
    When using Python3 the returned value of the commit is a byte string and
    therefore needs to be decoded.

--- wa/framework/version.py
+++ wa/framework/version.py
@@ -46,7 +46,7 @@ def get_commit():
     p.wait()
     if p.returncode:
         return None
-    if sys.version_info[0] == 3 and isinstance(std, str):
+    if sys.version_info[0] == 3 and isinstance(std, bytes):
         return std[:8].decode(sys.stdout.encoding)
     else:
         return std[:8]
