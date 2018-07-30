c3385fd9d826e1076d0e8e9e332b8afc6d98dd7c
Valentin Schneider <valentin.schneider@arm.com>
2018-07-30 14:36:37 +0100

    utils/env: Remove ShareState

--- libs/utils/env.py
+++ libs/utils/env.py
@@ -49,16 +49,11 @@ LATEST_LINK = 'results_latest'
 basepath = os.path.dirname(os.path.realpath(__file__))
 basepath = basepath.replace('/libs/utils', '')
 
-class ShareState(object):
-    __shared_state = {}
-
-    def __init__(self):
-        self.__dict__ = self.__shared_state
 IFCFG_BCAST_RE = re.compile(
     r'Bcast:(.*) '
 )
 
-class TestEnv(ShareState):
+class TestEnv(object):
     """
     Represents the environment configuring LISA, the target, and the test setup
 
