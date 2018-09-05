da5f2a5975ef57a34a7174446854f136136bef98
Douglas RAILLARD <douglas.raillard@arm.com>
2018-09-05 13:02:19 +0100

    events: remove "print" event
    The event matching infrastructure cannot cope with lines matching
    multiple events, unless there are only 2 events matching and only one of
    them is a fallback event. Unfortunately, TracingMarkWrite lines will
    always contain a "print:" substring, which makes it ambiguous with the
    Print event. Since no codebase is currently relying on the Print event,
    it is safe to remove it.

--- tests/test_fallback.py
+++ tests/test_fallback.py
@@ -18,24 +18,20 @@ import trappy
 from utils_tests import SetupDirectory
 
 TEST_DATA = """
-shutils-15864 [000] 27361.777314: tracing_mark_write: 0xffffff800819fac8s: cpu_frequency_devlib:        state=999000 cpu_id=0
-shutils-15864 [000] 27361.777428: tracing_mark_write: 0xffffff800819fac8s: cpu_frequency_devlib:        state=999000 cpu_id=1
-shutils-15864 [000] 27361.777523: tracing_mark_write: 0xffffff800819fac8s: cpu_frequency_devlib:        state=999000 cpu_id=2
-shutils-15864 [000] 27361.777616: tracing_mark_write: 0xffffff800819fac8s: cpu_frequency_devlib:        state=999000 cpu_id=3
-shutils-15864 [000] 27361.777709: tracing_mark_write: 0xffffff800819fac8s: cpu_frequency_devlib:        state=2362000 cpu_id=4
-shutils-15864 [000] 27361.777803: tracing_mark_write: 0xffffff800819fac8s: cpu_frequency_devlib:        state=2362000 cpu_id=5
-shutils-15864 [000] 27361.777896: tracing_mark_write: 0xffffff800819fac8s: cpu_frequency_devlib:        state=2362000 cpu_id=6
-shutils-15864 [000] 27361.777989: tracing_mark_write: 0xffffff800819fac8s: cpu_frequency_devlib:        state=2362000 cpu_id=7
-    sh-15888 [007] 27361.904411: tracing_mark_write: 0xffffff800819fac8s: TRACE_MARKER_START
-    sh-15895 [004] 27362.971605: tracing_mark_write: 0xffffff800819fac8s: TRACE_MARKER_STOP
-shutils-15900 [007] 27363.018426: tracing_mark_write: 0xffffff800819fac8s: cpu_frequency_devlib:        state=1844000 cpu_id=0
-shutils-15900 [007] 27363.018474: tracing_mark_write: 0xffffff800819fac8s: cpu_frequency_devlib:        state=1844000 cpu_id=1
-shutils-15900 [007] 27363.018506: tracing_mark_write: 0xffffff800819fac8s: cpu_frequency_devlib:        state=1844000 cpu_id=2
-shutils-15900 [007] 27363.018536: tracing_mark_write: 0xffffff800819fac8s: cpu_frequency_devlib:        state=1844000 cpu_id=3
-shutils-15900 [007] 27363.018566: tracing_mark_write: 0xffffff800819fac8s: cpu_frequency_devlib:        state=1805000 cpu_id=4
-shutils-15900 [007] 27363.018594: tracing_mark_write: 0xffffff800819fac8s: cpu_frequency_devlib:        state=1805000 cpu_id=5
-shutils-15900 [007] 27363.018623: tracing_mark_write: 0xffffff800819fac8s: cpu_frequency_devlib:        state=1805000 cpu_id=6
-shutils-15900 [007] 27363.018651: tracing_mark_write: 0xffffff800819fac8s: cpu_frequency_devlib:        state=1805000 cpu_id=7
+    bash-7842  [002]   354.414778: print:                tracing_mark_write: TRACE_MARKER_START
+ shutils-7843  [001]   354.590131: print:                tracing_mark_write: cpu_frequency_devlib:        state=950000 cpu_id=0
+ shutils-7843  [001]   354.590253: print:                tracing_mark_write: cpu_frequency_devlib:        state=1200000 cpu_id=1
+ shutils-7843  [001]   354.590369: print:                tracing_mark_write: cpu_frequency_devlib:        state=1200000 cpu_id=2
+ shutils-7843  [001]   354.590477: print:                tracing_mark_write: cpu_frequency_devlib:        state=950000 cpu_id=3
+ shutils-7843  [001]   354.590584: print:                tracing_mark_write: cpu_frequency_devlib:        state=950000 cpu_id=4
+ shutils-7843  [001]   354.590691: print:                tracing_mark_write: cpu_frequency_devlib:        state=950000 cpu_id=5
+ shutils-7868  [001]   357.732485: print:                tracing_mark_write: cpu_frequency_devlib:        state=950000 cpu_id=0
+ shutils-7868  [001]   357.732607: print:                tracing_mark_write: cpu_frequency_devlib:        state=1200000 cpu_id=1
+ shutils-7868  [001]   357.732726: print:                tracing_mark_write: cpu_frequency_devlib:        state=1200000 cpu_id=2
+ shutils-7868  [001]   357.732833: print:                tracing_mark_write: cpu_frequency_devlib:        state=950000 cpu_id=3
+ shutils-7868  [001]   357.732939: print:                tracing_mark_write: cpu_frequency_devlib:        state=950000 cpu_id=4
+ shutils-7868  [001]   357.733077: print:                tracing_mark_write: cpu_frequency_devlib:        state=950000 cpu_id=5
+    bash-7870  [002]   357.892659: print:                tracing_mark_write: TRACE_MARKER_STOP
 """
 
 class TestFallback(SetupDirectory):
@@ -48,15 +44,6 @@ class TestFallback(SetupDirectory):
 
         trace = trappy.FTrace(events=['cpu_frequency_devlib', 'tracing_mark_write'])
 
-        self.assertEqual(len(trace.cpu_frequency_devlib.data_frame), 16)
+        self.assertEqual(len(trace.cpu_frequency_devlib.data_frame), 12)
         self.assertEqual(len(trace.tracing_mark_write.data_frame), 2)
 
-    def test_print(self):
-        with open("trace.txt", "w") as fout:
-            data = TEST_DATA.replace('tracing_mark_write', 'print')
-            fout.write(data)
-
-        trace = trappy.FTrace(events=['cpu_frequency_devlib', 'print'])
-
-        self.assertEqual(len(trace.cpu_frequency_devlib.data_frame), 16)
-        self.assertEqual(len(trace.print_.data_frame), 2)
