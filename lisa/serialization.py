b59d106578c8a4fdb75708968ec46e892d3cff17
Douglas RAILLARD <douglas.raillard@arm.com>
2018-09-19 10:49:20 +0100

    serialization: Allow serialization customization
    Add some class attributes to allow customization of what content is
    serialized.

--- lisa/serialization.py
+++ lisa/serialization.py
@@ -1,6 +1,10 @@
 from ruamel.yaml import YAML
+import copy
 
 class YAMLSerializable:
+    serialized_whitelist = []
+    serialized_blacklist = []
+    serialized_placeholders = dict()
 
     _yaml = YAML(typ='unsafe')
     _yaml.allow_unicode = True
@@ -21,5 +25,56 @@ class YAMLSerializable:
     def from_path(cls, filepath):
         with open(filepath, "r") as fh:
             return cls.from_stream(fh)
+
+    #TODO: figure out why it is breaking deserialization of circular object
+    # graphs
+    #  def __getstate__(self):
+        #  """
+        #  Filter the instance's attributes upon serialization.
+
+        #  The following class attributes can be used to customize the serialized
+        #  content:
+            #  * :attr:`serialized_whitelist`: list of attribute names to
+              #  serialize. All other attributes will be ignored and will not be
+              #  saved/restored.
+
+            #  * :attr:`serialized_blacklist`: list of attribute names to not
+              #  serialize.  All other attributes will be saved/restored.
+
+            #  * serialized_placeholders: Map of attribute names to placeholder
+              #  values. These attributes will not be serialized, and the
+              #  placeholder value will be used upon restoration.
+
+            #  If both :attr:`serialized_whitelist` and
+            #  :attr:`serialized_blacklist` are specified,
+            #  :attr:`serialized_blacklist` is ignored.
+        #  """
+
+        #  dct = copy.copy(self.__dict__)
+        #  if self.serialized_whitelist:
+            #  dct = {attr: dct[attr] for attr in self.serialized_whitelist}
+
+        #  elif self.serialized_blacklist:
+            #  for attr in self.serialized_blacklist:
+                #  dct.pop(attr, None)
+
+        #  for attr, placeholder in self.serialized_placeholders.items():
+            #  dct.pop(attr, None)
+
+        #  return dct
+
+    #  def __setstate__(self, dct):
+        #  if self.serialized_placeholders:
+            #  dct.update(copy.deepcopy(self.serialized_placeholders))
+        #  self.__dict__ = dct
+
+    def __copy__(self):
+        """Make sure that copying the class still works as usual, without
+        dropping some attributes.
+        """
+        return super().__copy__(self)
+
+    def __deepcopy__(self):
+        return super().__deepcopy__()
 
 # vim :set tabstop=4 shiftwidth=4 textwidth=80 expandtab
