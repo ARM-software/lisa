1d137cd29c7fbcdb421c1f259ed0a90a74db6233
Douglas RAILLARD <douglas.raillard@arm.com>
2018-09-20 16:40:03 +0100

    serialization: Uncomment some code
    Since ruamel.yaml issue #238 is partially resolved, this code does not
    break the deserialization anymore.

--- lisa/serialization.py
+++ lisa/serialization.py
@@ -89,48 +89,45 @@ class Serializable:
         with open(str(filepath), **kwargs) as fh:
             return loader(fh)
 
-    #TODO: figure out why it is breaking deserialization of circular object
-    # graphs:
-    # https://bitbucket.org/ruamel/yaml/issues/238/constructorerror-with-recursive-objects
-    #  def __getstate__(self):
-        #  """
-        #  Filter the instance's attributes upon serialization.
+    def __getstate__(self):
+        """
+        Filter the instance's attributes upon serialization.
 
-        #  The following class attributes can be used to customize the serialized
-        #  content:
-            #  * :attr:`serialized_whitelist`: list of attribute names to
-              #  serialize. All other attributes will be ignored and will not be
-              #  saved/restored.
+        The following class attributes can be used to customize the serialized
+        content:
+            * :attr:`serialized_whitelist`: list of attribute names to
+              serialize. All other attributes will be ignored and will not be
+              saved/restored.
 
-            #  * :attr:`serialized_blacklist`: list of attribute names to not
-              #  serialize.  All other attributes will be saved/restored.
+            * :attr:`serialized_blacklist`: list of attribute names to not
+              serialize.  All other attributes will be saved/restored.
 
-            #  * serialized_placeholders: Map of attribute names to placeholder
-              #  values. These attributes will not be serialized, and the
-              #  placeholder value will be used upon restoration.
+            * serialized_placeholders: Map of attribute names to placeholder
+              values. These attributes will not be serialized, and the
+              placeholder value will be used upon restoration.
 
-            #  If both :attr:`serialized_whitelist` and
-            #  :attr:`serialized_blacklist` are specified,
-            #  :attr:`serialized_blacklist` is ignored.
-        #  """
+            If both :attr:`serialized_whitelist` and
+            :attr:`serialized_blacklist` are specified,
+            :attr:`serialized_blacklist` is ignored.
+        """
 
-        #  dct = copy.copy(self.__dict__)
-        #  if self.serialized_whitelist:
-            #  dct = {attr: dct[attr] for attr in self.serialized_whitelist}
+        dct = copy.copy(self.__dict__)
+        if self.serialized_whitelist:
+            dct = {attr: dct[attr] for attr in self.serialized_whitelist}
 
-        #  elif self.serialized_blacklist:
-            #  for attr in self.serialized_blacklist:
-                #  dct.pop(attr, None)
+        elif self.serialized_blacklist:
+            for attr in self.serialized_blacklist:
+                dct.pop(attr, None)
 
-        #  for attr, placeholder in self.serialized_placeholders.items():
-            #  dct.pop(attr, None)
+        for attr, placeholder in self.serialized_placeholders.items():
+            dct.pop(attr, None)
 
-        #  return dct
+        return dct
 
-    #  def __setstate__(self, dct):
-        #  if self.serialized_placeholders:
-            #  dct.update(copy.deepcopy(self.serialized_placeholders))
-        #  self.__dict__ = dct
+    def __setstate__(self, dct):
+        if self.serialized_placeholders:
+            dct.update(copy.deepcopy(self.serialized_placeholders))
+        self.__dict__ = dct
 
     def __copy__(self):
         """Make sure that copying the class still works as usual, without
