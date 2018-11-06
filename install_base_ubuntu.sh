54b8c5e501f5454f395d49da07b18f4013d02d02
Valentin Schneider <valentin.schneider@arm.com>
2018-11-06 17:56:48 +0000

    install_base_ubuntu: Add wget & expect

--- install_base_ubuntu.sh
+++ install_base_ubuntu.sh
@@ -61,8 +61,8 @@ done
 
 apt-get update
 
-apt-get -y install build-essential git python3 python3-pip python3-tk \
-	trace-cmd kernelshark
+apt-get -y install build-essential git wget expect trace-cmd kernelshark \
+	python3 python3-pip python3-tk
 
 # Upgrade pip so we can use wheel packages instead of compiling stuff, this is
 # much faster.
