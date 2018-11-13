c6f8fdb2c684ea05afbb70e9867a98a9d15744c2
Valentin Schneider <valentin.schneider@arm.com>
2018-11-13 18:02:23 +0000

    analysis: Cleanup cpu analysis
    Use the newly-introduced helper methods, add a load_tracking analysis
    module to move plotting methods out of the cpus module.

--- lisa/analysis/cpus.py
+++ lisa/analysis/cpus.py
@@ -21,6 +21,8 @@ import matplotlib.pyplot as plt
 import pylab as pl
 import pandas as pd
 
+from trappy.utils import handle_duplicate_index
+
 from lisa.analysis.base import AnalysisBase
 
 
@@ -48,19 +50,17 @@ class CpusAnalysis(AnalysisBase):
 
         :returns: :mod:`pandas.DataFrame`
         """
-        if not self._trace.hasEvents('sched_switch'):
-            self._log.warning('Events [sched_switch] not found, context switch '
-                              'computation not possible!')
-            return None
+        self.check_events(['sched_switch'])
 
         sched_df = self._trace.df_events('sched_switch')
-        cpus = list(range(self._trace.plat_info['cpus-count']))
+        cpus = list(range(self._trace.cpus_count))
         ctx_sw_df = pd.DataFrame(
             [len(sched_df[sched_df['__cpu'] == cpu]) for cpu in cpus],
             index=cpus,
             columns=['context_switch_cnt']
         )
         ctx_sw_df.index.name = 'cpu'
+
         return ctx_sw_df
 
     def df_cpu_wakeups(self, cpus=None):
@@ -74,10 +74,7 @@ class CpusAnalysis(AnalysisBase):
                   row shows a time when the given ``cpu`` was woken up from
                   idle.
         """
-        if not self._trace.hasEvents('cpu_idle'):
-            self._log.warning('Events [cpu_idle] not found, cannot '
-                              'get CPU wakeup events.')
-            return None
+        self.check_events(['cpu_idle'])
 
         cpus = cpus or list(range(self._trace.cpus_count))
 
@@ -90,127 +87,61 @@ class CpusAnalysis(AnalysisBase):
 
         return pd.DataFrame({'cpu': sr}).sort_index()
 
+    def signal_cpu_active(self, cpu):
+        """
+        Build a square wave representing the active (i.e. non-idle) CPU time,
+        i.e.:
+
+          cpu_active[t] == 1 if the CPU is reported to be non-idle by cpuidle at
+          time t
+          cpu_active[t] == 0 otherwise
+
+        :param cpu: CPU ID
+        :type cpu: int
+
+        :returns: A :class:`pandas.Series` or ``None`` if the trace contains no
+                  "cpu_idle" events
+        """
+        self.check_events(['cpu_idle'])
+
+        idle_df = self._trace.df_events('cpu_idle')
+        cpu_df = idle_df[idle_df.cpu_id == cpu]
+
+        cpu_active = cpu_df.state.apply(
+            lambda s: 1 if s == -1 else 0
+        )
+
+        start_time = 0.0
+        if not self._trace.ftrace.normalized_time:
+            start_time = self._trace.ftrace.basetime
+
+        if cpu_active.empty:
+            cpu_active = pd.Series([0], index=[start_time])
+        elif cpu_active.index[0] != start_time:
+            entry_0 = pd.Series(cpu_active.iloc[0] ^ 1, index=[start_time])
+            cpu_active = pd.concat([entry_0, cpu_active])
+
+        # Fix sequences of wakeup/sleep events reported with the same index
+        return handle_duplicate_index(cpu_active)
+
+
 ###############################################################################
 # Plotting Methods
 ###############################################################################
 
-    def plot_cpu(self, cpus=None):
-        """
-        Plot CPU-related signals for both big and LITTLE clusters.
-
-        :param cpus: list of CPUs to be plotted
-        :type cpus: list(int)
-        """
-        if not self._trace.hasEvents('sched_load_avg_cpu'):
-            self._log.warning('Events [sched_load_avg_cpu] not found, '
-                              'plot DISABLED!')
-            return
-
-        # Filter on specified cpus
-        if cpus is None:
-            cpus = sorted(self._big_cpus + self._little_cpus)
-
-        # Plot: big CPUs
-        bcpus = set(cpus).intersection(self._big_cpus)
-        if bcpus:
-            self._plot_cpu(bcpus, "big")
-
-        # Plot: LITTLE CPUs
-        lcpus = set(cpus).intersection(self._little_cpus)
-        if lcpus:
-            self._plot_cpu(lcpus, "LITTLE")
-
-
-###############################################################################
-# Utility Methods
-###############################################################################
-
-    def _plot_cpu(self, cpus, label=''):
-        """
-        Internal method that generates plots for all input CPUs.
-
-        :param cpus: list of CPUs to be plotted
-        :type cpus: list(int)
-        """
-        if label != '':
-            label1 = '{} '.format(label)
-            label2 = '_{}s'.format(label.lower())
-
-        # Plot required CPUs
-        _, pltaxes = plt.subplots(len(cpus), 1, figsize=(16, 3*(len(cpus))))
-
-        idx = 0
-        for cpu in cpus:
-
-            # Reference axes to be used
-            axes = pltaxes
-            if len(cpus) > 1:
-                axes = pltaxes[idx]
-
-            # Add CPU utilization
-            axes.set_title('{0:s}CPU [{1:d}]'.format(label1, cpu))
-            df = self._trace.df_events('sched_load_avg_cpu')
-            df = df[df.cpu == cpu]
-            if len(df):
-                df[['util_avg']].plot(ax=axes, drawstyle='steps-post',
-                                      alpha=0.4)
-
-            # if self._trace.hasEvents('sched_boost_cpu'):
-            #     df = self._trace.df_events('sched_boost_cpu')
-            #     df = df[df.cpu == cpu]
-            #     if len(df):
-            #         df[['usage', 'boosted_usage']].plot(
-            #             ax=axes,
-            #             style=['m-', 'r-'],
-            #             drawstyle='steps-post');
-
-            # Add Capacities data if avilable
-            if self._trace.hasEvents('cpu_capacity'):
-                df = self._trace.df_events('cpu_capacity')
-                df = df[df.cpu == cpu]
-                if len(df):
-                    # data = df[['capacity', 'tip_capacity', 'max_capacity']]
-                    # data.plot(ax=axes, style=['m', 'y', 'r'],
-                    data = df[['capacity', 'tip_capacity']]
-                    data.plot(ax=axes, style=['m', '--y'],
-                              drawstyle='steps-post')
-
-            # Add overutilized signal to the plot
-            self._trace.analysis.status.plot_overutilized(axes)
-
-            axes.set_ylim(0, 1100)
-            axes.set_xlim(self._trace.x_min, self._trace.x_max)
-
-            if idx == 0:
-                axes.annotate("{}CPUs Signals".format(label1),
-                              xy=(0, axes.get_ylim()[1]),
-                              xytext=(-50, 25),
-                              textcoords='offset points', fontsize=16)
-            # Disable x-axis timestamp for top-most cpus
-            if len(cpus) > 1 and idx < len(cpus)-1:
-                axes.set_xticklabels([])
-                axes.set_xlabel('')
-            axes.grid(True)
-
-            idx += 1
-
-        # Save generated plots into datadir
-        figname = '{}/{}cpus{}.png'.format(self._trace.plots_dir,
-                                           self._trace.plots_prefix, label2)
-        pl.savefig(figname, bbox_inches='tight')
-
-    def plot_context_switch(self):
+    def plot_context_switch(self, filepath=None):
         """
         Plot histogram of context switches on each CPU.
         """
-        if not self._trace.hasEvents('sched_switch'):
-            self._log.warning('Events [sched_switch] not found, plot DISABLED!')
-            return
+        fig, axis = self.setup_plot(height=8)
 
         ctx_sw_df = self.df_context_switches()
-        ax = ctx_sw_df.plot.bar(title="Per-CPU Task Context Switches",
-                                legend=False,
-                                figsize=(16, 8))
-        ax.grid()
+        ctx_sw_df.plot.bar(title="Per-CPU Task Context Switches",
+                           legend=False,
+                           ax=axis)
+        axis.grid()
+
+        self.save_plot(fig, filepath)
+        return axis
 
 # vim :set tabstop=4 shiftwidth=4 expandtab textwidth=80
---
+++ lisa/analysis/load_tracking.py
@@ -0,0 +1,119 @@
+# SPDX-License-Identifier: Apache-2.0
+#
+# Copyright (C) 2018, Arm Limited and contributors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+""" Scheduler load tracking analysis module """
+
+import matplotlib.pyplot as plt
+import pylab as pl
+import pandas as pd
+
+from lisa.analysis.base import AnalysisBase
+
+
+class LoadTrackingAnalysis(AnalysisBase):
+    """
+    Support for scheduler load tracking analysis
+
+    :param trace: input Trace object
+    :type trace: :class:`Trace`
+    """
+
+    name = 'load_tracking'
+
+    def __init__(self, trace):
+        super().__init__(trace)
+
+    def df_cpus(self):
+        """
+        A DataFrame containing per-CPU load tracking signals
+        """
+        try:
+            event = 'sched_load_cfs_rq'
+            self.check_events([event])
+        except RuntimeError:
+            event = 'sched_load_avg_cpu'
+            self.check_events([event])
+
+        df = self._trace.df_events(event)
+        if event == 'sched_cfs_rq':
+            df = df[df.path == '/']
+            df.drop('rbl_load')
+        else:
+            pass
+
+        return df
+
+    def df_tasks(self):
+        """
+        A DataFrame containing per-task load tracking signals
+        """
+        try:
+            event = 'sched_load_se'
+            self.check_events([event])
+        except RuntimeError:
+            event = 'sched_load_avg_task'
+            self.check_events([event])
+
+        df = self._trace.df_events(event)
+        if event == 'sched_load_se':
+            df = df[df.path == '(null)']
+        else:
+            pass
+
+        return df
+
+
+    def plot_cpus(self, filepath=None, cpus=None):
+        """
+        Plot CPU-related signals
+
+        :param cpus: list of CPUs to be plotted
+        :type cpus: list(int)
+        """
+        cpus = cpus or list(range(self._trace.cpus_count))
+        fig, axes = self.setup_plot(nrows=len(cpus), sharex=True)
+
+        cpus_df = self.df_cpus()
+
+        for cpu in cpus:
+            axis = axes[cpu] if len(cpus) > 1 else axes
+
+            # Add CPU utilization
+            axis.set_title('CPU{}'.format(cpu))
+            df = cpus_df[cpus_df.cpu == cpu]
+
+            if len(df):
+                df[['util']].plot(ax=axis, drawstyle='steps-post', alpha=0.4)
+                df[['load']].plot(ax=axis, drawstyle='steps-post', alpha=0.4)
+
+            # Add capacities data if available
+            if self._trace.hasEvents('cpu_capacity'):
+                df = self._trace.df_events('cpu_capacity')
+                df = df[df.cpu == cpu]
+                if len(df):
+                    data = df[['capacity', 'tip_capacity']]
+                    data.plot(ax=axis, style=['m', '--y'],
+                              drawstyle='steps-post')
+
+            # Add overutilized signal to the plot
+            self._trace.analysis.status.plot_overutilized(axis)
+
+            axis.set_ylim(0, 1100)
+            axis.set_xlim(self._trace.x_min, self._trace.x_max)
+
+        self.save_plot(fig, filepath)
+        return axes
