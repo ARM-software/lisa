b92f5340f078ef347f8d2b314c2b10b8deee7976
Quentin Perret <quentin.perret@arm.com>
2018-11-23 15:52:04 +0000

    install: Satisfy dependencies for pycairo
    lisa-install fails to install pycairo without libcairo2-dev on ubuntu
    18.04. Make sure to install it during the first setup.
    
    Signed-off-by: Quentin Perret <quentin.perret@arm.com>

--- install_base_ubuntu.sh
+++ install_base_ubuntu.sh
@@ -65,7 +65,8 @@ apt-get update
 # venv is not installed by default on Ubuntu, even though it is part of the
 # Python standard library
 apt-get -y install build-essential git wget expect kernelshark \
-	python3 python3-pip python3-venv python3-tk gobject-introspection
+	python3 python3-pip python3-venv python3-tk gobject-introspection \
+	libcairo2-dev
 
 if [ "$install_android_sdk" == y ]; then
     install_sdk
