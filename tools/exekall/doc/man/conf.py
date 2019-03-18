0eb5036512751421e13adff70464e083a9e180c9
Douglas RAILLARD <public.douglas.raillard@gmail.com>
2019-03-18 19:36:37 +0100

    exekall: add doc
    Add documentation in the form of a man page written in reStructuredText
    and rendered using Sphinx. That page is also included in LISA html doc.

---
+++ tools/exekall/doc/man/conf.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+#
+# Configuration file for the Sphinx documentation builder.
+#
+# This file does only contain a selection of the most common options. For a
+# full list see the documentation:
+# http://www.sphinx-doc.org/en/master/config
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
+
+# Base Sphinx configuration for manpages
+from lisa.doc.manconf import *
+
+# -- Project information -----------------------------------------------------
+
+project = 'exekall'
+
+# The short X.Y version
+version = ''
+# The full version, including alpha/beta/rc tags
+release = '1.0'
+
+# -- Options for manual page output ------------------------------------------
+
+# One entry per manual page. List of tuples
+# (source start file, name, description, authors, manual section).
+man_pages = [
+    (master_doc, 'exekall', 'test runner',
+     [author], 1)
+]
+
