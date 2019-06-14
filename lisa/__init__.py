c7437c645ba28905a6663c6185de8f2c06d23713
Douglas RAILLARD <douglas.raillard@arm.com>
2019-06-14 12:14:05 +0100

    lisa: Setup warnings filters
    * Raise an exception when a deprecated API is used from within "lisa"
    package
    * Always show warnings when a deprecated API is used from __main__,
    which includes notebook cells.
    
    This ensures LISA avoids using any deprecated APIs, both internally
    deprecated APIs and deprecated APIs from other modules. There seems to
    be no way of distinguishing both in the warning filters.

--- lisa/__init__.py
+++ lisa/__init__.py
@@ -1,3 +1,21 @@
 from lisa.version import __version__
 
+import warnings
+# Raise an exception when a deprecated API is used from within a lisa.*
+# submodule. This ensures that we don't use any deprecated APIs internally, so
+# they are only kept for external backward compatibility purposes.
+warnings.filterwarnings(
+    action='error',
+    category=DeprecationWarning,
+    module=r'{}\..*'.format(__name__),
+)
+
+# When the deprecated APIs are used from __main__ (script or notebook), always
+# show the warning
+warnings.filterwarnings(
+    action='always',
+    category=DeprecationWarning,
+    module=r'__main__',
+)
+
 # vim :set tabstop=4 shiftwidth=4 textwidth=80 expandtab
