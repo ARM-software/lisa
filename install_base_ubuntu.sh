5149728ce96adac88128fe6213e12ac32e6524c0
Douglas RAILLARD <douglas.raillard@arm.com>
2019-08-06 12:21:37 +0100

    install_base_ubuntu.sh: Sanitize indentation
    Use 4 spaces in the whole file instead of a mix of tab and spaces.

--- install_base_ubuntu.sh
+++ install_base_ubuntu.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#! /usr/bin/env bash
 
 # Script to install the depenencies for LISA on an Ubuntu-like system.
 
@@ -14,31 +14,31 @@ usage() {
 }
 
 latest_version() {
-	TOOL=${1}
-	$SCRIPT_DIR/tools/android-sdk-linux/tools/bin/sdkmanager --list  | \
-		awk "/ $TOOL/{VER=\$1}; END{print VER}"
+    TOOL=${1}
+    $SCRIPT_DIR/tools/android-sdk-linux/tools/bin/sdkmanager --list  | \
+        awk "/ $TOOL/{VER=\$1}; END{print VER}"
 }
 
 install_sdk() {
     apt-get -y install openjdk-8-jre openjdk-8-jdk
     mkdir -p "$SCRIPT_DIR"/tools
     if [ ! -e "$SCRIPT_DIR"/tools/android-sdk-linux ]; then
-	ANDROID_SDK_URL="https://dl.google.com/android/android-sdk_r24.4.1-linux.tgz"
-	echo "Downloading Android SDK [$ANDROID_SDK_URL]..."
-	wget -qO- $ANDROID_SDK_URL | tar xz -C $SCRIPT_DIR/tools/
-	# Find last version of required SDK tools
-	VER_BUILD_TOOLS=$(latest_version " build-tools")
-	VER_PLATFORM_TOOLS=$(latest_version " platform-tools")
-	VER_TOOLS=$(latest_version " tools")
-	expect -c "
-	    set timeout -1;
-	    spawn $SCRIPT_DIR/tools/android-sdk-linux/tools/android \
-		    update sdk --no-ui -t $VER_BUILD_TOOLS,$VER_PLATFORM_TOOLS,$VER_TOOLS
-	    expect {
-		\"Do you accept the license\" { exp_send \"y\r\" ; exp_continue }
-		eof
-	    }
-	"
+        ANDROID_SDK_URL="https://dl.google.com/android/android-sdk_r24.4.1-linux.tgz"
+        echo "Downloading Android SDK [$ANDROID_SDK_URL]..."
+        wget -qO- $ANDROID_SDK_URL | tar xz -C $SCRIPT_DIR/tools/
+        # Find last version of required SDK tools
+        VER_BUILD_TOOLS=$(latest_version " build-tools")
+        VER_PLATFORM_TOOLS=$(latest_version " platform-tools")
+        VER_TOOLS=$(latest_version " tools")
+        expect -c "
+            set timeout -1;
+            spawn $SCRIPT_DIR/tools/android-sdk-linux/tools/android \
+                update sdk --no-ui -t $VER_BUILD_TOOLS,$VER_PLATFORM_TOOLS,$VER_TOOLS
+            expect {
+            \"Do you accept the license\" { exp_send \"y\r\" ; exp_continue }
+            eof
+            }
+        "
     fi
 }
 
@@ -70,9 +70,9 @@ install_doc_extras=n
 
 for arg in "$@"; do
     if [ "$arg" == "--install-android-sdk" ]; then
-	install_android_sdk=y
+    install_android_sdk=y
     elif [ "$arg" == "--install-doc-extras" ]; then
-	install_doc_extras=y
+    install_doc_extras=y
     else
         echo "Unrecognised argument: $arg"
         usage
