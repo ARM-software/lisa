ARM-software/lisa

commit 48db507ca67b4e959f1c835f49f102e58da5a051

lisa.wlgen.rta: Use RT task for calibration if target is rooted
Restore feature that was disabled by mistake at some point.
Also freeze the target userspace when doing that.

lisa/wlgen/rta.py
@@ -329,23 +329,36 @@ class RTA(Workload):
         logger = cls.get_logger()
 
         # Create calibration task
-        max_rtprio = int(target.execute('ulimit -Hr').split('\r')[0])
-        logger.debug('Max RT prio: %d', max_rtprio)
+        if target.is_rooted:
+            max_rtprio = int(target.execute('ulimit -Hr').split('\r')[0])
+            logger.debug('Max RT prio: %d', max_rtprio)
 
-        if max_rtprio > 10:
-            max_rtprio = 10
+            priority = max_rtprio if max_rtprio <= 10 else 10
+            sched_policy = 'FIFO'
+        else:
+            logger.warning('Will use default scheduler class instead of RT since the target is not rooted')
+            priority = None
+            sched_policy = None
 
         for cpu in target.list_online_cpus():
             logger.info('CPU%d calibration...', cpu)
 
             # RT-app will run a calibration for us, so we just need to
             # run a dummy task and read the output
-            calib_task = Periodic(duty_cycle_pct=100, duration_s=0.001, period_ms=1)
+            calib_task = Periodic(
+                duty_cycle_pct=100,
+                duration_s=0.001,
+                period_ms=1,
+                priority=priority,
+                sched_policy=sched_policy,
+            )
             rta = cls.by_profile(target, name="rta_calib_cpu{}".format(cpu),
                                  profile={'task1': calib_task},
                                  calibration="CPU{}".format(cpu),
                                  res_dir=res_dir)
-            rta.run(as_root=True)
+
+            with target.freeze_userspace():
+                rta.run(as_root=True)
 
             for line in rta.output.split('\n'):
                 pload_match = re.search(pload_regexp, line)

commit 9b9871871eeb35ca5dd21b6fcb41b763dc11361f

Don't force running rta as root for calibration
target.freeze_tasks() needs to check if the device is rooted before
looking for the cgroup devlib module. This module is never present in
unrooted devices.

Only run rta as root if the device is rooted.

Fixes: 48db507ca67b4e959f1c835f49f102e58da5a051

lisa/target.py
@@ -722,34 +722,34 @@ class Target(Loggable, HideExekallID, ExekallTaggable, Configurable):
         Context manager that lets you freeze the userspace
         """
         logger = self.get_logger()
-        if 'cgroups' not in self.target.modules:
-            raise RuntimeError('Could not freeze userspace: "cgroups" devlib module is necessary')
-
-        controllers = [s.name for s in self.target.cgroups.list_subsystems()]
-        if 'freezer' not in controllers:
-            logger.warning('Could not freeze userspace: freezer cgroup controller not available on the target')
-            cm = nullcontext
-
-        elif not self.is_rooted:
+        if not self.is_rooted:
             logger.warning('Could not freeze userspace: target is not rooted')
             cm = nullcontext
-
         else:
-            exclude = copy.copy(self.CRITICAL_TASKS[self.target.os])
+            if 'cgroups' not in self.target.modules:
+                raise RuntimeError('Could not freeze userspace: "cgroups" devlib module is necessary')
 
-            # Do not freeze the process in charge of de-freezing, otherwise we
-            # will freeze to death and a machine hard reboot will be required
-            if isinstance(self.target, devlib.LocalLinuxTarget):
-                exclude.append(str(os.getpid()))
+            controllers = [s.name for s in self.target.cgroups.list_subsystems()]
+            if 'freezer' not in controllers:
+                logger.warning('Could not freeze userspace: freezer cgroup controller not available on the target')
+                cm = nullcontext
 
-            @contextlib.contextmanager
-            def cm():
-                logger.info('Freezing all tasks except: %s', ','.join(exclude))
-                try:
-                    yield self.target.cgroups.freeze(exclude)
-                finally:
-                    logger.info('Un-freezing userspace tasks')
-                    self.target.cgroups.freeze(thaw=True)
+            else:
+                exclude = copy.copy(self.CRITICAL_TASKS[self.target.os])
+
+                # Do not freeze the process in charge of de-freezing, otherwise we
+                # will freeze to death and a machine hard reboot will be required
+                if isinstance(self.target, devlib.LocalLinuxTarget):
+                    exclude.append(str(os.getpid()))
+
+                @contextlib.contextmanager
+                def cm():
+                    logger.info('Freezing all tasks except: %s', ','.join(exclude))
+                    try:
+                        yield self.target.cgroups.freeze(exclude)
+                    finally:
+                        logger.info('Un-freezing userspace tasks')
+                        self.target.cgroups.freeze(thaw=True)
 
         with cm() as x:
             yield x

lisa/wlgen/rta.py
@@ -403,7 +403,7 @@ class RTA(Workload):
                                  res_dir=res_dir)
 
             with rta, target.disable_idle_states(), target.freeze_userspace():
-                rta.run(as_root=True)
+                rta.run(as_root=target.is_rooted)
 
             for line in rta.output.split('\n'):
                 pload_match = re.search(pload_regexp, line)