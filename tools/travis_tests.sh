cf3defa59fe72bccf1ff7f376a652f0dc98227e1
Douglas RAILLARD <douglas.raillard@arm.com>
2019-10-31 10:53:34 +0000

    tools: travis_test.sh: Whitelist BART removal commit
    Commits touching external/ subtrees are not allowed to be merged in master, so
    whitelist this subtree removal.

--- tools/travis_tests.sh
+++ tools/travis_tests.sh
@@ -20,8 +20,9 @@
 # Script run by Travis. It is mostly a workaround for Travis inability to
 # correctly handle environment variable set in sourced scripts.
 
+commit_whitelist='496b859d1'
 illegal_location="external/"
-illegal_commits=$(find "$illegal_location" -mindepth 1 -maxdepth 1 -type d -print0 | xargs -0 git log --no-merges --oneline)
+illegal_commits=$(find "$illegal_location" -mindepth 1 -maxdepth 1 -type d -print0 | xargs -0 git log --no-merges --oneline | grep -v "$commit_whitelist")
 
 if [[ -n "$illegal_commits" ]]; then
     echo -e "The following commits are touching $illegal_location, which is not allowed apart from updates:\n$illegal_commits"
