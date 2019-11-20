1b97c38745d9c5c7f3481ba3867d95760c24040b
douglas-raillard-arm <public.douglas.raillard@gmail.com>
2019-11-20 20:31:24 +0000

    lisa.analysis.rta: Display on which CPU each rta phase ran
    Show the CPUs each phase ran on in labels of color bands drawn by
    RTAEventsAnalysis.plot_phases().

--- lisa/analysis/rta.py
+++ lisa/analysis/rta.py
@@ -22,7 +22,7 @@ from collections import namedtuple
 import pandas as pd
 
 from lisa.analysis.base import AnalysisHelpers, TraceAnalysisBase
-from lisa.datautils import df_filter_task_ids
+from lisa.datautils import df_filter_task_ids, df_window
 from lisa.trace import TaskID, requires_events
 from lisa.utils import memoized, deprecate
 
@@ -545,13 +545,28 @@ class RTAEventsAnalysis(TraceAnalysisBase):
         :type task: int or str or lisa.trace.TaskID
         """
         phases_df = self.df_phases(task)
+        loops_df = self.df_rtapp_loop(task)
+
+        def end_of_phase_at(t):
+            return phases_df['duration'][t]
+
+        def cpus_of_phase_at(t):
+            window = (t, end_of_phase_at(t))
+            df = df_window(loops_df, window, method='pre')
+            return sorted(int(x) for x in df['__cpu'].unique())
 
         # Compute phases intervals
-        bands = [(t, t + phases_df['duration'][t]) for t in phases_df.index]
-        for idx, (start, end) in enumerate(bands):
+        bands = [
+            (t, end_of_phase_at(t), cpus_of_phase_at(t))
+            for t in phases_df.index
+        ]
+
+        for idx, (start, end, cpus) in enumerate(bands):
             color = self.get_next_color(axis)
-            label = 'Phase_{:02d}'.format(idx)
+            cpus = ', '.join(map(str, cpus))
+            label = 'rt-app phase #{} (CPUs: {})'.format(idx, cpus)
             axis.axvspan(start, end, alpha=0.1, facecolor=color, label=label)
+
         axis.legend(loc='upper center', bbox_to_anchor=(0.5, -0.2,), ncol=8)
 
         if local_fig:
