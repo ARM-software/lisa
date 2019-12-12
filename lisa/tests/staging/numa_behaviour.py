4378b37d84aed96563f50073d608ae79d209a50d
Valentin Schneider <valentin.schneider@arm.com>
2019-12-12 11:16:41 +0000

    lisa.tests.staging.numa_behaviour: Docstring & whitespace cleanup

--- lisa/tests/staging/numa_behaviour.py
+++ lisa/tests/staging/numa_behaviour.py
@@ -41,26 +41,26 @@ class NUMABehaviour(RTATestBundle):
     @TasksAnalysis.df_task_states.used_events
     def _get_task_cpu_df(self, task_id):
         """
-        Get a DataFrame for task mirgrations
+        Get a DataFrame for task migrations
 
-        Use the sched_switch trace event to find task mirgation from one CPU to another.
+        Use the sched_switch trace event to find task migration from one CPU to another.
 
         :returns: A Pandas DataFrame for the task, showing the
                   CPU's that the task was migrated to
-        """        
+        """
         df = self.trace.analysis.tasks.df_task_states(task_id)
         cpu_df = df_deduplicate(df, cols=['cpu'], keep='first', consecutives=True)
-                
+
         return cpu_df
 
     @_get_task_cpu_df.used_events
     def test_task_remains(self) -> ResultBundle:
         """
-        Test that task remaines on the same core 
+        Test that task remains on the same core
         """
         test_passed = True
         metrics = {}
-        
+
         for task_id in self.rtapp_task_ids:
             cpu_df = self._get_task_cpu_df(task_id)
             core_migrations = len(cpu_df.index)
@@ -73,7 +73,7 @@ class NUMABehaviour(RTATestBundle):
 
         res = ResultBundle.from_bool(test_passed)
         res.add_metric("Migrations", metrics)
-        
+
         return res
 
 class NUMASmallTaskPlacement(NUMABehaviour):
@@ -93,25 +93,25 @@ class NUMASmallTaskPlacement(NUMABehaviour):
         )
 
         return rtapp_profile
-    
+
 class NUMAMultipleTasksPlacement(NUMABehaviour):
     """
     Multiple tasks with 50% utilization
     """
     task_prefix = "tsk"
-    
+
     @classmethod
     def get_rtapp_profile(cls, plat_info):
         # Four CPU's is enough to demonstrate task migration problem
-        cpu_count = min(4, plat_info["cpus-count"])        
+        cpu_count = min(4, plat_info["cpus-count"])
 
         rtapp_profile = {}
-        for cpu in range(cpu_count):            
+        for cpu in range(cpu_count):
             rtapp_profile["{}{}".format(cls.task_prefix, cpu)] = Periodic(
                 duty_cycle_pct=50,
                 duration_s=30,
                 period_ms=cls.TASK_PERIOD_MS
             )
 
-        return rtapp_profile    
+        return rtapp_profile
 # vim :set tabstop=4 shiftwidth=4 textwidth=80 expandtab
