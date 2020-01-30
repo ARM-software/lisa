58b126bfa5876dd8b8d1da7294fe252805e093ee
Douglas RAILLARD <douglas.raillard@arm.com>
2020-01-30 16:50:42 +0000

    lisa.datautils: Fix sched_load_cfs_rq SignalDesc to include cpu field

--- lisa/datautils.py
+++ lisa/datautils.py
@@ -1121,8 +1121,9 @@ _SIGNALS = [
 
     SignalDesc('sched_load_se', ['__comm', '__pid']),
     SignalDesc('sched_util_est_task', ['comm', 'pid']),
+
     SignalDesc('sched_util_est_cpu', ['cpu']),
-    SignalDesc('sched_load_cfs_rq', ['path']),
+    SignalDesc('sched_load_cfs_rq', ['path', 'cpu']),
     SignalDesc('sched_pelt_irq', ['cpu']),
     SignalDesc('sched_pelt_rt', ['cpu']),
     SignalDesc('sched_pelt_dl', ['cpu']),
