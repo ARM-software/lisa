3e9662c2f4731431014c17a12da0078d7bd52431
Douglas RAILLARD <douglas.raillard@arm.com>
2020-02-25 14:01:53 +0000

    lisa.wlgen.rta: Handle floating point duty cycles.

--- lisa/wlgen/rta.py
+++ lisa/wlgen/rta.py
@@ -902,8 +902,7 @@ class Ramp(RTATask):
         sign = +1 if start_pct <= end_pct else -1
         delta_pct = sign * abs(delta_pct)
 
-        steps = list(range(start_pct, end_pct + delta_pct, delta_pct))
-
+        steps = list(itertools.takewhile(lambda x: x <= end_pct, itertools.count(start_pct, delta_pct)))
         # clip the last step
         steps[-1] = end_pct
 
