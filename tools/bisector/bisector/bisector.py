166e5d08ab80067cdfe03c0276beea78490aac06
Douglas RAILLARD <douglas.raillard@arm.com>
2020-03-09 15:31:49 +0000

    tools: bisector: Fix YAML parser referencing

--- tools/bisector/bisector/bisector.py
+++ tools/bisector/bisector/bisector.py
@@ -2583,7 +2583,7 @@ def load_yaml(yaml_path):
     """Load the sequence of steps from a YAML file."""
     try:
         with open(yaml_path, 'r', encoding='utf-8') as f:
-            mapping = Report.yaml.load(f)
+            mapping = Report._get_yaml().load(f)
     except (ruamel.yaml.parser.ParserError, ruamel.yaml.scanner.ScannerError) as e:
         raise ValueError('Could not parse YAML file {yaml_path}: {e}'.format(**locals())) from e
 
