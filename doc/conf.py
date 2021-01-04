9386aa5cf31d1d2f1434ac97f4eb96cb33519b29
douglas-raillard-arm <douglas.raillard@gmail.com>
2021-01-04 14:51:37 +0000

    doc/conf.py: Remove :ivar: reference workaround
    Stopped working with Sphinx 3.4.0

--- doc/conf.py
+++ doc/conf.py
@@ -53,50 +53,6 @@ for name, obj in vars(unittest).items():
     except Exception:
         pass
 
-# This is a hack to prevent :ivar: docs from attempting to create a reference
-# https://github.com/sphinx-doc/sphinx/issues/2549
-# Credit goes to https://stackoverflow.com/a/41184353/5096023
-
-
-def patched_make_field(self, types, domain, items, env=None):
-    # type: (List, unicode, Tuple) -> nodes.field
-    def handle_item(fieldarg, content):
-        par = nodes.paragraph()
-        par += addnodes.literal_strong('', fieldarg)  # Patch: this line added
-        # par.extend(self.make_xrefs(self.rolename, domain, fieldarg,
-        #                           addnodes.literal_strong))
-        if fieldarg in types:
-            par += nodes.Text(' (')
-            # NOTE: using .pop() here to prevent a single type node to be
-            # inserted twice into the doctree, which leads to
-            # inconsistencies later when references are resolved
-            fieldtype = types.pop(fieldarg)
-            if len(fieldtype) == 1 and isinstance(fieldtype[0], nodes.Text):
-                typename = ''.join(n.astext() for n in fieldtype)
-                par.extend(self.make_xrefs(self.typerolename, domain, typename,
-                                           addnodes.literal_emphasis))
-            else:
-                par += fieldtype
-            par += nodes.Text(')')
-        par += nodes.Text(' -- ')
-        par += content
-        return par
-
-    fieldname = nodes.field_name('', self.label)
-    if len(items) == 1 and self.can_collapse:
-        fieldarg, content = items[0]
-        bodynode = handle_item(fieldarg, content)
-    else:
-        bodynode = self.list_type()
-        for fieldarg, content in items:
-            bodynode += nodes.list_item('', handle_item(fieldarg, content))
-    fieldbody = nodes.field_body('', bodynode)
-    return nodes.field('', fieldname, fieldbody)
-
-
-TypedField.make_field = patched_make_field
-
-
 RTD = (os.getenv('READTHEDOCS') == 'True')
 
 # For ReadTheDocs only: source init_env and get all env var defined by it.
