5acdad4ee1bdef55328e06e38e1d085910764998
douglas-raillard-arm <douglas.raillard@gmail.com>
2021-05-06 17:49:14 +0100

    lisa.trace: Use TraceBase.window when possible
    Use self.window instead of (self.start, self.end)

--- lisa/trace.py
+++ lisa/trace.py
@@ -2175,7 +2175,7 @@ class TraceView(Loggable, TraceBase):
         try:
             window = kwargs['window']
         except KeyError:
-            window = (self.start, self.end)
+            window = self.window
         kwargs['window'] = window
 
         return self.base_trace.df_event(event, **kwargs)
