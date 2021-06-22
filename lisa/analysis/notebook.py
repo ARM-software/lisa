8936ea2f283b2482d53480e9e9ea97eeea90cf68
Douglas Raillard <douglas.raillard@arm.com>
2021-06-22 15:57:44 +0100

    lisa.trace: Move Trace.df_all_events() to NotebookAnalysis
    Move to NotebookAnalysis so it can benefit from the dataframe cache, as
    it is a non-essential method of Trace class.
    
    A deprecated shim is left in place.

--- lisa/analysis/notebook.py
+++ lisa/analysis/notebook.py
@@ -24,7 +24,8 @@ import __main__ as main
 
 from lisa.analysis.base import TraceAnalysisBase
 from lisa.trace import requires_events
-from lisa.datautils import df_refit_index, df_filter, SignalDesc
+from lisa.datautils import df_refit_index, df_filter, SignalDesc, df_update_duplicates
+from lisa.utils import kwargs_forwarded_to
 
 
 class NotebookAnalysis(TraceAnalysisBase):
@@ -74,6 +75,95 @@ class NotebookAnalysis(TraceAnalysisBase):
             return val.__get__(self, type(self))
         else:
             return val
+
+    @TraceAnalysisBase.cache
+    def _df_all_events(self, events, field_sep=' ', fields_as_cols=None, event_as_col=True):
+        """
+        Split implementation to be able to use the cache
+        """
+        if fields_as_cols is None:
+            fields_as_cols = ['__comm', '__pid', '__cpu']
+        else:
+            fields_as_cols = list(fields_as_cols)
+
+        trace = self.trace
+
+        if not events:
+            return pd.DataFrame({'info': []})
+        else:
+            if event_as_col:
+                fmt = '{fields}'
+            else:
+                fmt = '{{event:<{max_len}}}: {{fields}}'.format(
+                    max_len=max(len(event) for event in events)
+                )
+
+            fields_as_cols_set = set(fields_as_cols)
+            def make_info_row(row, event):
+                fields = field_sep.join(
+                    f'{key}={value}'
+                    for key, value in row.iteritems()
+                    if key not in fields_as_cols_set
+                )
+                return fmt.format(
+                    event=event,
+                    fields=fields,
+                )
+
+            def make_info_df(event):
+                df = trace.df_event(event)
+                df = pd.DataFrame(
+                    {
+                        'info': df.apply(make_info_row, axis=1, event=event),
+                        **{
+                            field: df[field]
+                            for field in fields_as_cols
+                        }
+                    },
+                    index=df.index,
+                )
+
+                if event_as_col:
+                    df['event'] = event
+                return df
+
+            df = pd.concat(map(make_info_df, events) )
+            df.sort_index(inplace=True)
+            df_update_duplicates(df, inplace=True)
+            return df
+
+    @kwargs_forwarded_to(_df_all_events)
+    def df_all_events(self, events=None, **kwargs):
+        """
+        Provide a dataframe with an ``info`` column containing the textual
+        human-readable representation of the events fields.
+
+        :param events: List of events to include. If ``None``, all parsed
+            events will be used.
+
+            .. note:: Since events can be parsed on-demand, passing ``None``
+                might result in different results depending on what was done
+                with the object. For reproducible behaviour, pass an explicit
+                list of events.
+        :type events: list(str) or None
+
+        :param field_sep: String to use to separate fields.
+        :type field_sep: str
+
+        :param fields_as_cols: List of fields to keep as separate columns rather than
+            merged in the ``info`` column. If ``None``, will default to a fixed
+            set of columns.
+        :type fields_as_cols: list(str) or None
+
+        :param event_as_col: If ``True``, the event name is split in its own
+            column.
+        :type event_as_col: bool
+        """
+        if events is None:
+            events = sorted(self.trace.available_events)
+
+        return self._df_all_events(events=events, **kwargs)
+
 
     @TraceAnalysisBase.plot_method(return_axis=False)
     def plot_event_field(self, event: str, field: str, axis, local_fig, filter_columns=None, filter_f=None):
