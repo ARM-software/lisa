c9bac56ed52bcb698dabb407a56422bbc0c797d6
Kajetan Puchalski <kajetan.puchalski@arm.com>
2023-04-12 15:05:40 +0100

    lisa._assets.kmodules.sched_tp: Add device field to pixel6_emeter
    FEATURE
    
    Use the previously added 'private' field in order to pass down the ID of
    the device from which power meter measurements are coming and include
    that information in the pixel6_emeter trace event.
    
    Signed-off-by: Kajetan Puchalski <kajetan.puchalski@arm.com>

--- lisa/_assets/kmodules/sched_tp/ftrace_events.h
+++ lisa/_assets/kmodules/sched_tp/ftrace_events.h
@@ -336,25 +336,27 @@ TRACE_EVENT(sched_cpu_capacity,
 #define PIXEL6_EMETER_CHAN_NAME_MAX_SIZE 64
 
 TRACE_EVENT(pixel6_emeter,
-	TP_PROTO(unsigned long ts, unsigned int chan, char *chan_name, unsigned long value),
-	TP_ARGS(ts, chan, chan_name, value),
+	TP_PROTO(unsigned long ts, unsigned int device, unsigned int chan, char *chan_name, unsigned long value),
+	TP_ARGS(ts, device, chan, chan_name, value),
 
 	TP_STRUCT__entry(
 		__field(unsigned long,		ts			)
 		__field(unsigned long,		value			)
+		__field(unsigned int,		device			)
 		__field(unsigned int,		chan			)
 		__array(char,			chan_name,	PIXEL6_EMETER_CHAN_NAME_MAX_SIZE	)
 	),
 
 	TP_fast_assign(
-		__entry->ts		= ts;
+		__entry->ts		    = ts;
+		__entry->device		= device;
 		__entry->chan		= chan;
 		__entry->value		= value;
 		strlcpy(__entry->chan_name, chan_name, PIXEL6_EMETER_CHAN_NAME_MAX_SIZE);
 	),
 
-	TP_printk("ts=%lu chan=%u chan_name=%s value=%lu",
-		  __entry->ts, __entry->chan, __entry->chan_name, __entry->value)
+	TP_printk("ts=%lu device=%u chan=%u chan_name=%s value=%lu",
+		  __entry->ts, __entry->device, __entry->chan, __entry->chan_name, __entry->value)
 );
 
 #endif /* _FTRACE_EVENTS_H */
