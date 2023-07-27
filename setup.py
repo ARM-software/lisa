0922fb5b3530cb23764d32a77b728eeafe6aab4a
Douglas Raillard <douglas.raillard@arm.com>
2023-07-27 15:20:46 +0100

    setup.py: Bump required Python to >= 3.8
    FEATURE
    
    Require Python >= 3.8 so that the typing standard module contains the
    necessary helpers.
    
    Python 3.7 has reached end of life and 2 Ubuntu LTS versions have been
    released with >= 3.8 so no breakage is expected. A number of packages
    like scipy have moved away from 3.7 already.

--- setup.py
+++ setup.py
@@ -96,7 +96,7 @@ extras_require['all'] = sorted(set(
     itertools.chain.from_iterable(extras_require.values())
 ))
 
-python_requires = '>= 3.7'
+python_requires = '>= 3.8'
 
 if __name__ == "__main__":
 
