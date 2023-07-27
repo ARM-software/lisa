cfe68d695dc33fcd04466eb0ca229e9bbfc04365
Douglas Raillard <douglas.raillard@arm.com>
2023-07-27 10:49:53 +0100

    lisa._generic: Remove custom hierarchy
    Remove custom type hierarchy in favor of the typing standard module.
    
    The only type hint that is left is SortedList, which is integrated to
    the typeguard package for runtime checking.

--- lisa/_generic.py
+++ lisa/_generic.py
@@ -20,193 +20,130 @@ Generic types inspired by the :mod:`typing` module.
 """
 
 import functools
-from collections.abc import Mapping, Sequence
-from operator import attrgetter
+import inspect
+from typing import Any, Union, Generic, TypeVar, List
+import typeguard
+from collections.abc import Iterable
 
-from lisa.utils import sphinx_register_nitpick_ignore
+from lisa.utils import get_cls_name
 
-def _isinstance(x, type_):
-    if isinstance(type_, tuple):
-        return any(map(lambda type_: _isinstance(x, type_), type_))
-    elif isinstance(type_, type):
-        return isinstance(x, type_)
-    # Typing hint
+class _TypeguardCustom:
+    _HINT = Any
+
+    @classmethod
+    def _instancecheck(cls, value):
+        return
+
+    @classmethod
+    def _typeguard_checker(cls, value, origin_type, args, memo):
+        typeguard.check_type_internal(value, cls._HINT, memo)
+
+        try:
+            cls._instancecheck(value)
+        except TypeError as e:
+            raise typeguard.TypeCheckError(str(e))
+
+
+def _typeguard_lookup(origin_type, args, extras):
+    try:
+        issub =  issubclass(origin_type, _TypeguardCustom)
+    except Exception:
+        issub = False
+
+    if issub:
+        return origin_type._typeguard_checker
     else:
-        try:
-            from typing import get_origin, get_args, Union
-        except ImportError:
-            # We cannot process the typing hint in that version of Python, so
-            # we assume the input is correctly typed. It's not ideal but we
-            # cannot do much more than that.
-            return True
-        else:
-            combinator = get_origin(type_)
-            args = get_args(type_)
-            if combinator == Union:
-                return any(map(lambda type_: _isinstance(x, type_), args))
-            else:
-                raise TypeError(f'Cannot handle type hint: {type_}')
+        return None
 
-class MetaBase(type):
-    def __instancecheck__(cls, instance):
-        try:
-            cls.instancecheck(instance)
-        except TypeError:
-            return False
-        else:
-            return True
-
-    # Fully memoize the function so that this always holds:
-    # assert Container[Foo] is Container[Foo]
-    @functools.lru_cache(maxsize=None, typed=True)
-    def __getitem__(cls, type_):
-        NewClass = cls.getitem(type_)
-
-        NewClass.__module__ = cls.__module__
-
-        # Since this type name is not resolvable, avoid cross reference
-        # warnings from Sphinx
-        sphinx_register_nitpick_ignore(NewClass)
-        return NewClass
+typeguard.checker_lookup_functions.append(_typeguard_lookup)
 
 
-class GenericContainerMetaBase(MetaBase):
+def check_type(x, classinfo):
     """
-    Base class for the metaclass of generic containers.
-
-    They are parameterized with the ``type_`` class attribute, and classes can
-    also be created by indexing on classes with :class:`GenericBase`
-    metaclass. The ``type_`` class attribute will be set with what is passed as
-    the key.
+    Equivalent of ``isinstance()`` that will also work with typing hints.
     """
+    if isinstance(classinfo, Iterable):
+        typ = Union[tuple(classinfo)]
+    else:
+        typ = classinfo
 
-    # Fully memoize the function so that this always holds:
-    # assert Container[Foo] is Container[Foo]
-    def getitem(cls, type_):
-        class NewClass(cls):
-            _type = type_
-
-        types = type_ if isinstance(type_, Sequence) else [type_]
-
-        def make_name(self_getter, sub_getter):
-            def _sub_getter(type_):
-                try:
-                    return sub_getter(type_)
-                # type hints like typing.Union don't have a name we can introspect,
-                # but it can be pretty-printed
-                except AttributeError:
-                    return str(type_)
-            return '{}[{}]'.format(
-                self_getter(cls),
-                ','.join(_sub_getter(type_) for type_ in types)
-            )
-
-        NewClass.__name__ = make_name(
-            attrgetter('__name__'),
-            attrgetter('__name__')
+    try:
+        typeguard.check_type(
+            value=x,
+            expected_type=typ,
+            forward_ref_policy=typeguard.ForwardRefPolicy.ERROR,
+            collection_check_strategy=typeguard.CollectionCheckStrategy.ALL_ITEMS,
         )
-
-        def type_param_name(t):
-            if t.__module__ == 'builtins':
-                return t.__qualname__
-            else:
-                # Add the module name so that Sphinx can establish cross
-                # references
-                return f'{t.__module__}.{t.__qualname__}'
-
-        NewClass.__qualname__ = make_name(
-            attrgetter('__qualname__'),
-            type_param_name,
-        )
-        return NewClass
+    except typeguard.TypeCheckError as e:
+        raise TypeError(str(e))
 
 
-class GenericBase:
+def is_instance(obj, classinfo):
     """
-    Base class for generic containers.
+    Same as builtin ``isinstance()`` but works with type hints.
     """
-    def __new__(cls, obj):
-        cls.instancecheck(obj)
-        return obj
+    try:
+        check_type(obj, classinfo)
+    except TypeError:
+        return False
+    else:
+        return True
 
 
-class GenericMappingMeta(GenericContainerMetaBase, type(Mapping)):
+def is_hint(obj):
     """
-    Metaclass for generic mapping containers.
+    Heuristic to check if a given ``obj`` is a typing hint or anything else.
+    This function will return ``False`` for classes.
 
-    It provides an ``__instancecheck__`` implementation that checks the type
-    of the keys and values. This make it suitable for input sanitizing based
-    on type checking.
+    .. warning:: Since there is currently no way to identify hints for sure,
+        the check might return ``False`` even if it is a hint.
     """
-    def instancecheck(cls, instance):
-        if not isinstance(instance, Mapping):
-            raise TypeError('not a Mapping')
+    module = getattr(obj, '__module__', None)
 
-        k_type, v_type = cls._type
-        for k, v in instance.items():
-            if not _isinstance(k, k_type):
-                raise TypeError(f'Key "{k}" of type {type(k).__qualname__} should be of type {k_type.__qualname__}', k)
-
-            if not _isinstance(v, v_type):
-                raise TypeError(f'Value of {type(v).__qualname__} key "{k}" should be of type {v_type.__qualname__}', k)
+    # This is a class, so cannot be a hint.
+    if isinstance(obj, type):
+        return issubclass(obj, _TypeguardCustom)
+    elif module in ('typing', 'typing_extensions'):
+        return True
+    else:
+        return False
 
 
-class TypedDict(GenericBase, dict, metaclass=GenericMappingMeta):
+@functools.lru_cache(maxsize=None, typed=True)
+def hint_to_class(hint):
     """
-    Subclass of dict providing keys and values type check.
+    Convert a typing hint to a class that will do a runtime check against the
+    hint when ``isinstance()`` is used.
     """
+    class Meta(type):
+        def __instancecheck__(cls, instance):
+            return is_instance(instance, hint)
+
+    class Stub(metaclass=Meta):
+        pass
+
+    name = get_cls_name(hint).split('.', 1)
+    try:
+        name = name[1]
+    except IndexError:
+        name = name[0]
+
+    Stub.__qualname__ = name
+    Stub.__name__ = name.split('.')[-1]
+
+    return Stub
 
 
-class GenericSequenceMeta(GenericContainerMetaBase, type(Sequence)):
-    """Similar to :class:`GenericMappingMeta` for sequences"""
-    def instancecheck(cls, instance):
-        if not isinstance(instance, Sequence):
-            raise TypeError('not a Sequence')
+T = TypeVar('T')
+class SortedList(Generic[T], _TypeguardCustom):
+    """
+    Same as :class:`typing.List` but enforces sorted values when runtime
+    checked using :mod:`typeguard`.
+    """
+    _HINT = List[T]
 
-        type_ = cls._type
-        for i, x in enumerate(instance):
-            if not _isinstance(x, type_):
-                raise TypeError(f'Item #{i} "{x}" of type {type(x).__qualname__} should be of type {type_.__qualname__}', i)
-
-class GenericSortedSequenceMeta(GenericSequenceMeta):
-    def instancecheck(cls, instance):
-        super().instancecheck(instance)
-        for i, (x, y) in enumerate(zip(instance, instance[1:])):
+    @classmethod
+    def _instancecheck(cls, value):
+        for i, (x, y) in enumerate(zip(value, value[1:])):
             if x > y:
                 raise TypeError(f'Item #{i} "{x}" is higher than the next item "{y}", but the list must be sorted')
-
-
-class TypedList(GenericBase, list, metaclass=GenericSequenceMeta):
-    """
-    Subclass of list providing keys and values type check.
-    """
-
-
-class SortedTypedList(GenericBase, list, metaclass=GenericSortedSequenceMeta):
-    """
-    Subclass of list providing keys and values type check, and also check the
-    list is sorted in ascending order.
-    """
-
-
-class OneOfMeta(MetaBase):
-    def getitem(cls, allowed):
-        class NewClass(cls):
-            _allowed = allowed
-
-        NewClass.__qualname__ = f'{cls.__qualname__}[{", ".join(map(repr, allowed))}]'
-        return NewClass
-
-    def instancecheck(cls, instance):
-        allowed = cls._allowed
-        if instance not in allowed:
-            raise ValueError(f'Value {repr(instance)} is not allowed. It must be one of: {", ".join(map(repr, allowed))}')
-
-
-class OneOf(GenericBase, metaclass=OneOfMeta):
-    """
-    Check that the provided value is part of a specific set of allowed values.
-    """
-    def __new__(cls, obj):
-        cls.instancecheck(obj)
-        return obj
