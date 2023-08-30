0d912c6b1ec7fd2cc41ea9f5721f3e130d909fdf
Douglas Raillard <douglas.raillard@arm.com>
2023-08-30 12:12:33 +0100

    lisa._assets.kmodules.lisa: Show compilation warning when introspection info is not available
    FEATURE
    
    Ensure the log contains a mention of the missing introspection
    information when appropriate for debugging.

--- lisa/_assets/kmodules/lisa/introspection.h
+++ lisa/_assets/kmodules/lisa/introspection.h
@@ -4,8 +4,21 @@
 
 #include <generated/private_types.h>
 
-#define HAS_TYPE(typ) ((defined(_TYPE_INTROSPECTION_INFO_AVAILABLE) && defined(_TYPE_EXISTS_##typ)) || (!defined(_TYPE_INTROSPECTION_INFO_AVAILABLE)))
-#define HAS_MEMBER(typ, member) ((defined(_TYPE_INTROSPECTION_INFO_AVAILABLE) && defined(_TYPE_HAS_MEMBER_##typ##___##member)) || (!defined(_TYPE_INTROSPECTION_INFO_AVAILABLE)))
-#define HAS_SYMBOL(name) ((defined(_SYMBOL_INTROSPECTION_INFO_AVAILABLE) && defined(_SYMBOL_EXISTS_##name)) || (!defined(_SYMBOL_INTROSPECTION_INFO_AVAILABLE)))
+#ifdef _TYPE_INTROSPECTION_INFO_AVAILABLE
+#    define HAS_TYPE(typ) defined(_TYPE_EXISTS_##typ)
+#    define HAS_MEMBER(typ, member) (HAS_TYPE(typ) && defined(_TYPE_HAS_MEMBER_##typ##___##member))
+#else
+#    warning "Type introspection information not available, HAS_TYPE() and HAS_MEMBER() will assume types and members exist"
+#    define HAS_TYPE(typ) (1)
+#    define HAS_MEMBER(typ, member) (1)
+#endif
+
+
+#ifdef _SYMBOL_INTROSPECTION_INFO_AVAILABLE
+#    define HAS_SYMBOL(name) defined(_SYMBOL_EXISTS_##name)
+#else
+#    warning "Symbol introspection information not available, HAS_SYMBOL() will assume all symbols exist"
+#    define HAS_SYMBOL(name) (1)
+#endif
 
 #endif /* INTROSPECTION_H */
