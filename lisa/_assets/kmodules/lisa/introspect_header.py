fe76a62cf680b9ad96e1d335c894fccfe834b787
Douglas Raillard <douglas.raillard@arm.com>
2023-08-31 12:08:22 +0100

    lisa._assets.kmodules.lisa: Fix introspect_header.py
    FIX
    
    * handling of typedef.
    * performance on large header.

--- lisa/_assets/kmodules/lisa/introspect_header.py
+++ lisa/_assets/kmodules/lisa/introspect_header.py
@@ -2,7 +2,7 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-# Copyright (C) 2019, Arm Limited and contributors.
+# Copyright (C) 2023, Arm Limited and contributors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
 # not use this file except in compliance with the License.
@@ -23,6 +23,7 @@ import itertools
 import argparse
 import subprocess
 from collections import namedtuple
+import functools
 
 from pycparser import c_parser, c_ast
 
@@ -33,69 +34,148 @@ class Record(abc.ABC):
         pass
 
 
-class TypeMemberRecord(namedtuple('TypeMemberRecord', ['type_name', 'member_name']), Record):
+class TypeMemberRecord(namedtuple('TypeMemberRecord', ['type_kind', 'type_name', 'member_name']), Record):
     def make_define(self):
-        return f'#define _TYPE_HAS_MEMBER_{self.type_name}___{self.member_name}'
-
-    def is_printable(self):
-        return self.type_name and self.member_name
+        return f'#define _TYPE_HAS_MEMBER_{self.type_kind}_{self.type_name}_LISA_SEPARATOR_{self.member_name}'
 
 
-class TypeExistsRecord(namedtuple('TypeExistsRecord', ['type_name']), Record):
+class TypeExistsRecord(namedtuple('TypeExistsRecord', ['type_kind', 'type_name']), Record):
     def make_define(self):
-        return f'#define _TYPE_EXISTS_{self.type_name}'
-
-    def is_printable(self):
-        return bool(self.type_name)
+        return f'#define _TYPE_EXISTS_{self.type_kind}_{self.type_name}'
 
 
-def expand_typ(typ):
-    if isinstance(typ, c_ast.TypeDecl):
-        return expand_typ(typ.type)
-    else:
-        return [typ]
+class TypedefMemo:
+    def __init__(self, types):
+        self.complete_memo = {}
+        self.resolved = {}
+
+        # Seed the memo so that we know complete type defintions when typedef
+        # is created based on a forward definition.
+        def walk(typ):
+            self.register_complete(typ)
+            self.resolve_typ(typ)
+
+        for typ in types:
+            walk(typ)
+
+    @staticmethod
+    def _get_key(typ):
+        if isinstance(typ, (c_ast.Struct, c_ast.Union, c_ast.Enum)):
+            return (typ.__class__, typ.name)
+        else:
+            raise ValueError('unhandled type')
+
+    @staticmethod
+    def _is_complete(typ):
+        if isinstance(typ, (c_ast.Struct, c_ast.Union)):
+            return typ.decls is not None
+        elif isinstance(typ, c_ast.Enum):
+            return typ.values is not None
+        else:
+            return True
+
+    def register_complete(self, typ):
+        try:
+            key = self._get_key(typ)
+        except ValueError:
+            return
+        else:
+            # Only memoize complete definitions, i.e. not forward definitions
+            if self._is_complete(typ):
+                self.complete_memo[key] = typ
+
+    def get_complete(self, typ):
+        try:
+            key = self._get_key(typ)
+        except ValueError:
+            return typ
+        else:
+            return self.complete_memo.get(key, typ)
 
 
-def resolve_typ(typ):
-    if isinstance(typ, (c_ast.Struct, c_ast.Union)):
-        children = typ.decls or []
-        children_typs = [
-            child.type
-            for child in children
-        ]
-        name = typ.name
-    elif isinstance(typ, c_ast.Enum):
-        children = typ.values or []
-        children_typs = []
-        name = typ.name
-    elif isinstance(typ, c_ast.TypeDecl):
-        name = typ.declname
-        _, children, children_typs = resolve_typ(typ.type)
-    else:
-        raise ValueError('Unhandled type')
+    def resolve_typ(self, typ):
+        try:
+            return self.resolved[typ]
+        except KeyError:
+            pass
 
-    return (name, children, children_typs)
+        recurse = self.resolve_typ
+
+        if isinstance(typ, (c_ast.Struct, c_ast.Union)):
+            def expand_child(decl):
+                typ = decl.type
+                # An struct/union anonymous member inside a struct/union is
+                # expanded in its parent.
+                if decl.name is None:
+                    _, _, children, children_typs = recurse(typ)
+                    return (children, list(children_typs) + [typ])
+                else:
+                    return ([decl], [typ.type])
+
+            name = typ.name
+            children = typ.decls
+
+            if children:
+                children, children_typs = map(itertools.chain.from_iterable, zip(*map(expand_child, children)))
+            else:
+                children = []
+                children_typs = []
+
+            if isinstance(typ, c_ast.Struct):
+                kind = 'struct'
+            elif isinstance(typ, c_ast.Union):
+                kind = 'union'
+            else:
+                raise ValueError(f'Unhandled type: {typ}')
+
+        elif isinstance(typ, c_ast.Enum):
+            children = typ.values or []
+            children_typs = []
+            name = typ.name
+            kind = 'enum'
+        elif isinstance(typ, c_ast.Typedef):
+            name = typ.name
+            # Typedef is refering to a type by name, so look the full definition of
+            # that type
+            child_typ = self.get_complete(typ.type.type)
+            _, _, children, children_typs = recurse(child_typ)
+            kind = 'typedef'
+        else:
+            return (None, None, [], [])
+
+        res = (kind, name, tuple(children), tuple(children_typs))
+        self.resolved[typ] = res
+        return res
 
 
-def walk_type(typ):
-    try:
-        name, children, children_typs = resolve_typ(typ)
-    except ValueError:
-        return []
-    else:
-        children_typs = itertools.chain.from_iterable(map(expand_typ, children_typs))
+def make_records(memo, types):
 
-        return itertools.chain(
-            [
-                TypeExistsRecord(type_name=name)
-            ],
-            (
-                TypeMemberRecord(type_name=name, member_name=child.name)
-                for child in children
-            ),
-            itertools.chain.from_iterable(map(walk_type, children_typs))
+    def recurse_multi(types):
+        return itertools.chain.from_iterable(
+            map(recurse, types)
         )
 
+    visited = set()
+    def recurse(typ):
+        if typ in visited:
+            return tuple()
+        else:
+            visited.add(typ)
+
+            kind, name, children, children_typs = memo.resolve_typ(typ)
+            return itertools.chain(
+                (
+                    TypeExistsRecord(type_kind=kind, type_name=name),
+                ) if name else tuple(),
+                (
+                    TypeMemberRecord(type_kind=kind, type_name=name, member_name=child.name)
+                    for child in children
+                    if name and child.name
+                ),
+                recurse_multi(children_typs)
+            )
+
+    return recurse_multi(types)
 
 def process_header(path):
     with open(path, 'r') as f:
@@ -112,13 +192,19 @@ def process_header(path):
 
     assert isinstance(node, c_ast.FileAST)
 
+    def expand_decl(node):
+        if isinstance(node, c_ast.Decl):
+            return node.type
+        else:
+            return node
+
     types = [
-        _node.type
+        expand_decl(_node)
         for _node in node
-        if isinstance(_node, (c_ast.Decl, c_ast.Typedef, c_ast.TypeDecl))
     ]
 
-    records = set(itertools.chain.from_iterable(map(walk_type, types)))
+    memo = TypedefMemo(types)
+    records = make_records(memo, types)
     return itertools.chain(
         (
             '#define _TYPE_INTROSPECTION_INFO_AVAILABLE',
@@ -126,11 +212,8 @@ def process_header(path):
         (
             record.make_define()
             for record in records
-            if record.is_printable()
         ),
     )
-
-
 
 
 class SymbolRecord(namedtuple('SymbolRecord', ['name']), Record):
@@ -184,16 +267,13 @@ def main():
 
     out = []
     if args.header:
-        out.extend(process_header(args.header))
+        out.append(process_header(args.header))
 
     if args.kallsyms:
-        out.extend(process_kallsyms(args.kallsyms))
+        out.append(process_kallsyms(args.kallsyms))
 
-    print('\n'.join(sorted(
-        s
-        for s in out
-        if s
-    )))
+    for rec in sorted(set(itertools.chain.from_iterable(out))):
+        print(rec)
 
 if __name__ == '__main__':
     main()
