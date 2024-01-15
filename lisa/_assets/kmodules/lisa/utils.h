c9a9ecd9107ea97898e20f08cce3168531102108
Chris Swinchatt <christopher.swinchatt@arm.com>
2024-01-15 11:40:34 +0000

    lisa._assets.kmodules.lisa: Add STRINGIFY_ALL and CLAMP utility macros
    Signed-off-by: Chris Swinchatt <christopher.swinchatt@arm.com>

--- lisa/_assets/kmodules/lisa/utils.h
+++ lisa/_assets/kmodules/lisa/utils.h
@@ -12,4 +12,24 @@
 
 #include "linux/kernel.h"
 
+#define __STRINGIFY_12(X, ...)	__stringify(X), __STRINGIFY_11(__VA_ARGS__)
+#define __STRINGIFY_11(X, ...)	__stringify(X), __STRINGIFY_10(__VA_ARGS__)
+#define __STRINGIFY_10(X, ...)	__stringify(X), __STRINGIFY_9(__VA_ARGS__)
+#define __STRINGIFY_9(X, ...)	__stringify(X), __STRINGIFY_8(__VA_ARGS__)
+#define __STRINGIFY_8(X, ...)	__stringify(X), __STRINGIFY_7(__VA_ARGS__)
+#define __STRINGIFY_7(X, ...)	__stringify(X), __STRINGIFY_6(__VA_ARGS__)
+#define __STRINGIFY_6(X, ...)	__stringify(X), __STRINGIFY_5(__VA_ARGS__)
+#define __STRINGIFY_5(X, ...)	__stringify(X), __STRINGIFY_4(__VA_ARGS__)
+#define __STRINGIFY_4(X, ...)	__stringify(X), __STRINGIFY_3(__VA_ARGS__)
+#define __STRINGIFY_3(X, ...)	__stringify(X), __STRINGIFY_2(__VA_ARGS__)
+#define __STRINGIFY_2(X, ...)	__stringify(X), __STRINGIFY_1(__VA_ARGS__)
+#define __STRINGIFY_1(X)	__stringify(X)
+#define __STRINGIFY_0()
+
+/** Stringify up to 12 args. */
+#define STRINGIFY_ALL(...)	CONCATENATE(__STRINGIFY_, COUNT_ARGS(__VA_ARGS__))(__VA_ARGS__)
+
+/** Clamp X to a value in [A,B]. */
+#define CLAMP(X, A, B)		((X) < (A) ? (A) : ((X) > (B) ? (B) : (X)))
+
 #endif /* _UTILS_H */
