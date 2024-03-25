091fdcdb9e915e3d2868b7fb560c457f93439414
Douglas Raillard <douglas.raillard@arm.com>
2024-03-25 13:45:37 +0000

    lisa.utils: Make memoized decorator thread-safe
    FIX

--- lisa/utils.py
+++ lisa/utils.py
@@ -632,38 +632,42 @@ def _lru_memoized(first_param_maxsize, other_params_maxsize, sig_f):
             insertion_counter = 0
             insertion_order = WeakKeyDictionary()
 
+            lock = threading.Lock()
+
             @functools.wraps(f)
             def wrapper(first, *args, **kwargs):
                 nonlocal insertion_counter
-                try:
-                    partial = cache_map[first]
-                except KeyError:
-                    # Only keep a weak reference here for the "partial" closure
-                    ref = weakref.ref(first)
 
-                    # This partial function does not take "first" as parameter, so
-                    # that the lru_cache will not keep a reference on it
-                    @apply_lru
-                    def partial(*args, **kwargs):
-                        return f(ref(), *args, **kwargs)
+                with lock:
+                    try:
+                        partial = cache_map[first]
+                    except KeyError:
+                        # Only keep a weak reference here for the "partial" closure
+                        ref = weakref.ref(first)
 
-                    cache_map[first] = partial
-                    insertion_order[first] = insertion_counter
-                    insertion_counter += 1
+                        # This partial function does not take "first" as parameter, so
+                        # that the lru_cache will not keep a reference on it
+                        @apply_lru
+                        def partial(*args, **kwargs):
+                            return f(ref(), *args, **kwargs)
 
-                    # Delete the caches for objects that are too old
-                    if first_param_maxsize is not None:
-                        # Make sure the content of insertion_order will not
-                        # change while iterating over it
-                        to_remove = [
-                            val
-                            for val, counter in insertion_order.items()
-                            if insertion_counter - counter > first_param_maxsize
-                        ]
+                        cache_map[first] = partial
+                        insertion_order[first] = insertion_counter
+                        insertion_counter += 1
 
-                        for val in to_remove:
-                            del cache_map[val]
-                            del insertion_order[val]
+                        # Delete the caches for objects that are too old
+                        if first_param_maxsize is not None:
+                            # Make sure the content of insertion_order will not
+                            # change while iterating over it
+                            to_remove = [
+                                val
+                                for val, counter in insertion_order.items()
+                                if insertion_counter - counter > first_param_maxsize
+                            ]
+
+                            for val in to_remove:
+                                del cache_map[val]
+                                del insertion_order[val]
 
                 return partial(*args, **kwargs)
 
