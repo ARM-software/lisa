3e344c4c8516eea130819deb77f9ca1302089b01
project_444_bot_b970213545be4e6c0185c64ae9fc5226 <project_444_bot_b970213545be4e6c0185c64ae9fc5226@noreply.gitlab.arm.com>
2024-09-20 11:56:34 +0000

    Autocommit to preview branch on Fri Sep 20 11:56:34 UTC 2024 tracking preview-force

--- setup.py
+++ setup.py
@@ -138,7 +138,7 @@ if __name__ == "__main__":
             "colorcet",
             # Avoid:
             # polars 1.7.0: https://github.com/pola-rs/polars/issues/18719
-            "polars >= 1.0.0, < 2.0.0, != 1.7.0",
+            "polars >= 1.0.0, < 2.0.0, < 1.7.0",
             # Pandas >= 1.0.0 has support for new nullable dtypes
             # Pandas 1.2.0 has broken barplots:
             # https://github.com/pandas-dev/pandas/issues/38947
