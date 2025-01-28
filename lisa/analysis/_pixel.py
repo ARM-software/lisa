7457d9bf399ed813da10ccdd111da06bec0c0770
project_444_bot_07c92c61a7e6a3ceb601937355c7b6b5 <project_444_bot_07c92c61a7e6a3ceb601937355c7b6b5@noreply.gitlab.arm.com>
2025-01-28 16:39:32 +0000

    Autocommit to master branch on Tue Jan 28 16:39:32 UTC 2025 tracking master-force

--- lisa/analysis/_pixel.py
+++ lisa/analysis/_pixel.py
@@ -17,12 +17,14 @@
 """
 Pixel phones specific analysis.
 """
+from typing import List, Optional
 
 import pandas as pd
+import polars as pl
 import holoviews as hv
 from holoviews import opts
 
-from lisa.datautils import df_add_delta
+from lisa.datautils import SignalDesc
 from lisa.analysis.base import TraceAnalysisBase
 from lisa.trace import requires_events
 from lisa.notebook import plot_signal
@@ -49,45 +51,79 @@ class PixelAnalysis(TraceAnalysisBase):
 
             * A ``channel`` column (name of the power meter channel)
             * A ``power`` column (average power usage in mW since the last measurement)
+            * A ``energy`` column (energy samples in mJ provided by the PMIC)
         """
-        df = self.trace.df_event('pixel6_emeter')
-        df = df[df['chan_name'].isin(self.EMETER_CHAN_NAMES)]
-        grouped = df.groupby('chan_name', observed=True, group_keys=False)
+        name_map = self.EMETER_CHAN_NAMES
+        trace = self.trace.get_view(df_fmt='polars-lazyframe')
 
-        def make_chan_df(df):
-            energy_diff = df_add_delta(df, col='energy_diff', src_col='value', window=self.trace.window)['energy_diff']
-            ts_diff = df_add_delta(df, col='ts_diff', src_col='ts', window=self.trace.window)['ts_diff']
-            power = energy_diff / ts_diff
-            df = pd.DataFrame(dict(power=power, channel=df['chan_name']))
-            return df.dropna()
+        signals = [
+            SignalDesc('pixel6_emeter', ['chan_name']),
+        ]
+        df = trace.df_event('pixel6_emeter', signals=signals)
+        df = df.rename({'value': 'energy'})
+        df = df.filter(pl.col('chan_name').is_in(name_map.keys()))
 
-        df = grouped[df.columns].apply(make_chan_df)
-        df['channel'] = df['channel'].astype('category').cat.rename_categories(self.EMETER_CHAN_NAMES)
+        nrg_diff = pl.col('energy').diff()
+        ts_diff = pl.col('ts').diff()
+        chan = pl.col('chan_name')
+        df = df.with_columns(
+            power=(nrg_diff / ts_diff).over('chan_name'),
+            channel=chan.replace_strict(name_map, default=chan),
+        )
 
-        return df
+        return df.select(('Time', 'channel', 'energy', 'power'))
 
     ###############################################################################
     # Plotting methods
     ###############################################################################
     @TraceAnalysisBase.plot_method
     @df_power_meter.used_events
-    def plot_power_meter(self, channels=None):
+    def plot_power_meter(self, channels: Optional[List[str]] = None, metrics: Optional[List[str]] = None):
         """
         Plot the power meter readings from various channels.
 
         :param channels: List of channels to plot
         :type channels: list(str)
 
+        :param metrics: List of metrics to plot. Can be:
+            * ``"power"``: plot the power (mW)
+            * ``"energy"``: plot the energy (mJ)
+        :type metrics: list(str)
+
         The channels needs to correspond to values in the ``channel`` column of df_power_meter().
         """
-        df = self.df_power_meter()
+        all_metrics = {
+            'power': 'mW',
+            'energy': 'mJ',
+        }
+        metrics = sorted(['power'] if metrics is None else metrics)
+        channels = channels or sorted(self.EMETER_CHAN_NAMES.values())
 
-        channels = list(channels or df['channel'].unique())
-        if any(channel not in df['channel'].cat.categories for channel in channels):
-            raise ValueError('Specified channel not found')
+        def check_allowed(kind, values, allowed):
+            forbidden = set(values) - set(allowed)
+            if forbidden:
+                forbidden = ', '.join(sorted(forbidden))
+                raise ValueError(f'{kind} names not recognized: {forbidden}')
 
-        channel_data = dict(iter(df[df['channel'].isin(channels)].groupby('channel', group_keys=False, observed=True)))
+        check_allowed('Channel', channels, self.EMETER_CHAN_NAMES.values())
+        check_allowed('Metrics', metrics, all_metrics.keys())
+
+        df = self.df_power_meter(df_fmt='polars-lazyframe')
+        df = df.filter(pl.col('channel').is_in(channels))
+        df = df.select(('Time', *metrics, 'channel'))
+        df = df.collect()
+        per_channel = df.partition_by('channel', include_key=False, as_dict=True)
+
         return hv.Overlay([
-            plot_signal(channel_data[channel]['power'], name=channel, vdim=hv.Dimension('power', label='Power', unit='mW'))
-            for channel in channels
-        ]).opts(title='Power usage per channel over time')
+            plot_signal(
+                df.select(('Time', metric)),
+                name=f'{channel} {metric}',
+                vdim=hv.Dimension(metric, label=metric.title(), unit=all_metrics[metric]),
+                window=self.trace.window,
+            )
+            for (channel,), df in sorted(per_channel.items())
+            for metric in sorted(metrics)
+        ]).opts(
+            title='Power usage per channel over time',
+            multi_y=True,
+        )
