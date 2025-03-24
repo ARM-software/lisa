e6f0758a6d9073397ba6d2ef6aff1be42e07c30f
Douglas Raillard <douglas.raillard@arm.com>
2025-03-24 13:33:03 +0000

    lisa._assets.kmodules.lisa: Add workqueue bindings
    FEATURE

---
+++ lisa/_assets/kmodules/lisa/rust/lisakmod/src/features/wq.rs
@@ -0,0 +1,36 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+use alloc::{sync::Arc, vec::Vec};
+
+pub use crate::runtime::wq::*;
+use crate::{features::define_feature, lifecycle::new_lifecycle};
+
+#[derive(Debug)]
+pub struct WqService {
+    wq: Wq,
+}
+
+impl WqService {
+    fn new() -> WqService {
+        WqService { wq: Wq::new() }
+    }
+
+    pub fn wq(&self) -> &Wq {
+        &self.wq
+    }
+}
+
+define_feature! {
+    pub struct WqFeature,
+    name: "__wq",
+    visibility: Private,
+    Service: WqService,
+    Config: (),
+    dependencies: [],
+    init: |configs| {
+        Ok(new_lifecycle!(|services| {
+            yield_!(Ok(Arc::new(WqService::new())));
+            Ok(())
+        }))
+    },
+}
